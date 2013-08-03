93c0646e4f861a72e9db08419e8189c6e13b6e87
Praveen Kumar Pendyala <praveendath92@gmail.com>
2013-08-04 01:07:14 +0530

    Version 2.8 Beta
    Former-commit-id: d7d383a69299bd550793ad56ce558e95eff5249d

--- src/in/co/praveenkumar/forums.java
+++ src/in/co/praveenkumar/forums.java
@@ -442,6 +442,15 @@ public class forums extends Activity {
 					// statusMessage.setText("value is " + i[0]);
 				}
 			});
+			
+			Button submitRatingLaterButton = (Button) dialog
+					.findViewById(R.id.submitRatingLater);
+
+			submitRatingLaterButton.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+						dialog.dismiss();
+				}
+			});
 			break;
 
 		case 3:
