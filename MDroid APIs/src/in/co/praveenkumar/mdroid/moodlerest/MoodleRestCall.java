93c77fbff03c3493fa90d6fd776cf35b9fe56161
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-03-27 12:56:29 +0530

    MoodleRestCall class added
    This does all Rest calls

---
+++ MDroid APIs/src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestCall.java
@@ -0,0 +1,56 @@
+package in.co.praveenkumar.mdroid.moodlerest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.util.Log;
+
+public class MoodleRestCall {
+	private final String DEBUG_TAG = "MoodleRestCall";
+
+	public String fetchContent(String mUrl, String params) {
+		String response = null;
+
+		Log.d(DEBUG_TAG, mUrl + params);
+		HttpURLConnection con;
+		try {
+			con = (HttpURLConnection) new URL(mUrl + params).openConnection();
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Accept", "application/xml");
+			con.setRequestProperty("Content-Language", "en-US");
+			con.setDoOutput(true);
+
+			OutputStreamWriter writer = new OutputStreamWriter(
+					con.getOutputStream());
+			writer.write("");
+			writer.flush();
+			writer.close();
+
+			// Get Response
+			StringBuilder buffer = new StringBuilder();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				buffer.append(line);
+				buffer.append('\r');
+			}
+			reader.close();
+			response = buffer.toString();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return response;
+	}
+
+}
