fc1ebc448e87d26391261a86aeb4cbeee0dd8cb3
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-03-29 03:33:01 +0530

    added navigation bar
    Still testing.

--- MDroid APIs/gen/in/co/praveenkumar/mdroid/apis/R.java
+++ MDroid APIs/gen/in/co/praveenkumar/mdroid/apis/R.java
@@ -21,13 +21,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int content_frame=0x7f080001;
+        public static final int drawer_layout=0x7f080000;
+        public static final int left_drawer=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_moodle_home=0x7f030001;
+        public static final int drawer_list_item=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -35,6 +42,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int drawer_close=0x7f050004;
+        public static final int drawer_open=0x7f050003;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
