50df90322e99c6ed620563d552fa52a3f84d3f71
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-03-29 21:14:30 +0530

    Drawer layout as LinearLayout instead of ListView

--- MDroid APIs/src/in/co/praveeenkumar/mdroid/extenders/DrawerActivity.java
+++ MDroid APIs/src/in/co/praveeenkumar/mdroid/extenders/DrawerActivity.java
@@ -28,7 +28,7 @@ public class DrawerActivity extends Activity {
 
 		mPlanetTitles = new String[] { "one", "two", "three" };
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+		mDrawerList = (ListView) findViewById(R.id.left_nav_listview);
 
 		// Set the adapter for the list view
 		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
