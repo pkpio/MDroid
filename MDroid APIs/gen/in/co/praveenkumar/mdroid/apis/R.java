2ab52483777d56d61bd51c585374fa4ebc62e3f9
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-03-29 21:14:30 +0530

    Drawer layout as LinearLayout instead of ListView
    Former-commit-id: 50df90322e99c6ed620563d552fa52a3f84d3f71

--- MDroid APIs/gen/in/co/praveenkumar/mdroid/apis/R.java
+++ MDroid APIs/gen/in/co/praveenkumar/mdroid/apis/R.java
@@ -47,13 +47,14 @@ containing a value of this type.
         public static final int nav_drawer_list_view_item_bg=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
+        public static final int action_settings=0x7f090007;
         public static final int content_frame=0x7f090001;
         public static final int drawer_layout=0x7f090000;
         public static final int left_drawer=0x7f090003;
+        public static final int left_nav_listview=0x7f090004;
         public static final int list=0x7f090002;
-        public static final int right_drawer=0x7f090004;
-        public static final int text1=0x7f090005;
+        public static final int right_drawer=0x7f090005;
+        public static final int text1=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
