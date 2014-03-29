3836d48a2c3466dd45860f453459e04407d39d73
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-03-29 12:46:09 +0530

    DrawerActivity for all navDrawer work
    Former-commit-id: b0cac563c20169d7fe0a1a2185e1ca7086f1d0d8

--- MDroid APIs/gen/in/co/praveenkumar/mdroid/apis/R.java
+++ MDroid APIs/gen/in/co/praveenkumar/mdroid/apis/R.java
@@ -27,10 +27,11 @@ public final class R {
         public static final int nav_drawer_list_view_item_bg=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080004;
         public static final int content_frame=0x7f080001;
         public static final int drawer_layout=0x7f080000;
         public static final int left_drawer=0x7f080002;
+        public static final int right_drawer=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
