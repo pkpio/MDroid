a3e4d78e44bd9a3691e43c00310e57d255d06cef
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-04-01 15:55:55 +0530

    MoodleCourseContent model complete

--- MDroid APIs/src/in/co/praveenkumar/mdroid/models/MoodleCourseContent.java
+++ MDroid APIs/src/in/co/praveenkumar/mdroid/models/MoodleCourseContent.java
@@ -1,5 +1,27 @@
 package in.co.praveenkumar.mdroid.models;
 
+import java.util.ArrayList;
+
 public class MoodleCourseContent {
+	ArrayList<MoodleCourseSection> sections;
+
+	public ArrayList<MoodleCourseSection> getSections() {
+		return sections;
+	}
+
+	public void setSections(ArrayList<MoodleCourseSection> sections) {
+		this.sections = sections;
+	}
+
+	public void addSection(MoodleCourseSection section) {
+		sections.add(section);
+	}
+
+	public MoodleCourseSection getSection(int index) {
+		if (index < sections.size())
+			return sections.get(index);
+		else
+			return null;
+	}
 
 }
