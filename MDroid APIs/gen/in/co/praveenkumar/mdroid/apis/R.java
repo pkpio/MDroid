a431de42744e880f33605070f6491609f84bd85b
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-04-04 04:35:30 +0530

    Design changes in ContentListItem
    Former-commit-id: 485183f8af9b78f07b5db7ad6224edce0e98f0f7

--- MDroid APIs/gen/in/co/praveenkumar/mdroid/apis/R.java
+++ MDroid APIs/gen/in/co/praveenkumar/mdroid/apis/R.java
@@ -79,11 +79,12 @@ containing a value of this type.
     }
     public static final class drawable {
         public static final int card=0x7f020000;
-        public static final int dashed_line=0x7f020001;
-        public static final int developer=0x7f020002;
-        public static final int drawer_shadow=0x7f020003;
-        public static final int ic_drawer=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
+        public static final int content_file_bg=0x7f020001;
+        public static final int dashed_line=0x7f020002;
+        public static final int developer=0x7f020003;
+        public static final int drawer_shadow=0x7f020004;
+        public static final int ic_drawer=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0014;
