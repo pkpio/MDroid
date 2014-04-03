9534c9b7b4539c4c3399b305233198a4ac1f51c2
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-04-03 14:01:58 +0530

    Fixed a nullpointer in MoodleCourseContent
    Former-commit-id: 672120f9d8d1637e987ae3160bd139c3b2368fcc

--- MDroid APIs/src/in/co/praveenkumar/mdroid/activities/CourseContentActivity.java
+++ MDroid APIs/src/in/co/praveenkumar/mdroid/activities/CourseContentActivity.java
@@ -5,6 +5,7 @@ import in.co.praveeenkumar.mdroid.extenders.StickyListViewAdapter;
 import in.co.praveenkumar.mdroid.apis.R;
 import in.co.praveenkumar.mdroid.models.MoodleCourseContent;
 import in.co.praveenkumar.mdroid.models.MoodleCourseModule;
+import in.co.praveenkumar.mdroid.models.MoodleCourseSection;
 import in.co.praveenkumar.mdroid.moodlerest.MoodleRestCourseContents;
 
 import java.util.ArrayList;
@@ -13,14 +14,16 @@ import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class CourseContentActivity extends DrawerActivity {
 	private final String DEBUG_TAG = "CourseContentActivity";
 	private MoodleCourseContent mCourseContent;
-	private ArrayList<MoodleCourseModule> mModules = new ArrayList<MoodleCourseModule>();
-	private ArrayList<String> sectionHeaders = new ArrayList<String>();
+	ArrayList<MoodleCourseSection> mSections = new ArrayList<MoodleCourseSection>();
+	ArrayList<MoodleCourseModule> mModules = new ArrayList<MoodleCourseModule>();
+	private contentListViewAdapter myListAdapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,15 @@ public class CourseContentActivity extends DrawerActivity {
 		listview = (StickyListHeadersListView) findViewById(R.id.course_content_list);
 		listview.setAreHeadersSticky(true);
 
+		// Build a string array of module section names
+		ArrayList<String> mSectionNames = new ArrayList<String>();
+		for (int i = 0; i < mModules.size(); i++)
+			mSectionNames.add(mModules.get(i).getSectionname());
+
+		// StickyHeader List view
+		myListAdapter = new contentListViewAdapter(this, mSectionNames);
+		listview.setAdapter(myListAdapter);
+
 	}
 
 	private class AsyncContentFetch extends AsyncTask<String, Integer, Long> {
@@ -42,6 +54,13 @@ public class CourseContentActivity extends DrawerActivity {
 			MoodleRestCourseContents mrcc = new MoodleRestCourseContents(
 					getApplicationContext());
 			mCourseContent = mrcc.getCourseContent("2");
+
+			Log.d(DEBUG_TAG, "Course json parsing complete.");
+
+			// Getting modules in all sections
+			for (int i = 0; i < mCourseContent.getSections().size(); i++) {
+				mModules.addAll(mCourseContent.getSection(i).getModules());
+			}
 			return null;
 
 		}
