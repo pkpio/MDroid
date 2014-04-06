f9164ac28d09762adad9c5cb4535d21b02c25eee
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-04-07 05:20:49 +0530

    Validation checking added for new EditText types

--- MDroid APIs/src/in/co/praveenkumar/mdroid/activities/LoginActivity.java
+++ MDroid APIs/src/in/co/praveenkumar/mdroid/activities/LoginActivity.java
@@ -8,12 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.andreabaccega.widget.FormEditText;
+
 public class LoginActivity extends Activity {
 	final String DEBUG_TAG = "LoginActivity";
 	Button switchNormal;
 	Button switchParanoid;
 	LinearLayout normalLayout;
 	LinearLayout paranoidLayout;
+	FormEditText mNormUrl;
+	FormEditText mParaUrl;
+	FormEditText unameView;
+	FormEditText pswdView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +33,29 @@ public class LoginActivity extends Activity {
 		switchParanoid = (Button) findViewById(R.id.switch_paranoid);
 		normalLayout = (LinearLayout) findViewById(R.id.normal_layout);
 		paranoidLayout = (LinearLayout) findViewById(R.id.paranoid_layout);
+		mNormUrl = (FormEditText) findViewById(R.id.norm_url);
+		mParaUrl = (FormEditText) findViewById(R.id.para_url);
+		unameView = (FormEditText) findViewById(R.id.username);
+		pswdView = (FormEditText) findViewById(R.id.password);
+
 		View v = null;
 		setToNormal(v);
+	}
+
+	public void doNormLogin(View v) {
+		FormEditText[] allFields = { mNormUrl, unameView, pswdView };
+
+		boolean allValid = true;
+		for (FormEditText field : allFields) {
+			allValid = field.testValidity() && allValid;
+		}
+
+		if (allValid) {
+			// YAY
+		} else {
+			// EditText are going to appear with an exclamation mark and an
+			// explicative message.
+		}
 	}
 
 	public void setToNormal(View v) {
