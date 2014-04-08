ed7c753eafc93a19716b6db5ca234cb392ebd946
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-04-08 23:52:23 +0530

    In-built downloader added to download options
    Former-commit-id: 277b3d1a9eb9bcb3fcdb169067d52b53cf9c60f8

--- MDroid APIs/src/in/co/praveenkumar/mdroid/helpers/MDroidDownloader.java
+++ MDroid APIs/src/in/co/praveenkumar/mdroid/helpers/MDroidDownloader.java
@@ -1,9 +1,16 @@
 package in.co.praveenkumar.mdroid.helpers;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.app.DownloadManager;
 import android.app.DownloadManager.Request;
 import android.content.Context;
 import android.net.Uri;
+import android.os.Environment;
 
 public class MDroidDownloader {
 	public final Boolean SYSTEM_DOWNLOADER = true;
@@ -15,7 +22,8 @@ public class MDroidDownloader {
 				.getSystemService(Context.DOWNLOAD_SERVICE);
 	}
 
-	public void download(String fileUrl, Boolean visibility, Boolean choice) {
+	public void download(String fileUrl, String fileName, Boolean visibility,
+			Boolean choice) {
 		if (choice == SYSTEM_DOWNLOADER) {
 			Request request = new Request(Uri.parse(fileUrl));
 			if (!visibility)
@@ -24,11 +32,35 @@ public class MDroidDownloader {
 				request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
 			// Request and save it's value
 		} else {
-
+			mdroidDownload(fileUrl, fileName);
 		}
 	}
 
-	public void setOptions() {
+	public void mdroidDownload(String fUrl, String fName) {
+		InputStream input = null;
+		OutputStream output = null;
+		HttpURLConnection connection = null;
+		try {
+			URL url = new URL(fUrl);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.connect();
 
+			// download the file
+			input = connection.getInputStream();
+			output = new FileOutputStream(
+					Environment.getExternalStorageDirectory() + "/MDroid/"
+							+ fName);
+
+			byte data[] = new byte[4096];
+			int count;
+			while ((count = input.read(data)) != -1) {
+				output.write(data, 0, count);
+			}
+
+			output.close();
+			input.close();
+		} catch (Exception e) {
+			// File download failed.
+		}
 	}
 }
