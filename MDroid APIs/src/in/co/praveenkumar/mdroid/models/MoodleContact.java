c9621db5fe6060f065538d5df680e2a6ef4018c1
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-04-09 05:20:33 +0530

    New model: MoodleContact
    Former-commit-id: c821510c237d4392970040e9f5ba34f0b948c20a

---
+++ MDroid APIs/src/in/co/praveenkumar/mdroid/models/MoodleContact.java
@@ -0,0 +1,50 @@
+package in.co.praveenkumar.mdroid.models;
+
+public class MoodleContact {
+	String id;
+	String fullname;
+	String imageurl;
+	int unread;
+	String status;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getImageurl() {
+		return imageurl;
+	}
+
+	public void setImageurl(String imageurl) {
+		this.imageurl = imageurl;
+	}
+
+	public int getUnread() {
+		return unread;
+	}
+
+	public void setUnread(int unread) {
+		this.unread = unread;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
