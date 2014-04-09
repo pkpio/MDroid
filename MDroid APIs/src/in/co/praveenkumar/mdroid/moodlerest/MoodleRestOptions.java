84d5a4c450c4edb626185b668f95738091044ad9
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-04-09 05:52:52 +0530

    MoodleRestContacts created and complete
    Former-commit-id: 9c0d7f39426cc800c32b0e9f123e2191f023985a

--- MDroid APIs/src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestOptions.java
+++ MDroid APIs/src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestOptions.java
@@ -21,6 +21,7 @@ public class MoodleRestOptions {
 	public static final String FUNCTION_GET_FORUMS = "mod_forum_get_forums_by_courses";
 	public static final String FUNCTION_GET_DISCUSSIONS = "mod_forum_get_forum_discussions";
 	public static final String FUNCTION_GET_SITE_INFO = "core_webservice_get_site_info";
+	public static final String FUNCTION_GET_CONTACTS = "core_message_get_contacts";
 
 	// Response strings
 	public static final String RESPONSE_AUTH_FAILED = "username was not found";
