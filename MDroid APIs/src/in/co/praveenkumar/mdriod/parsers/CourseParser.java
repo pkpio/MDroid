5a111089a417a3460a1b9f2bd658ca2ea8acb6e3
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-04-15 22:08:48 +0530

    CourseParser: Added conditions for TAG exits checking
    Former-commit-id: d21634e7c44a607905d17f962cd39bf7e8b3cf08

--- MDroid APIs/src/in/co/praveenkumar/mdriod/parsers/CourseParser.java
+++ MDroid APIs/src/in/co/praveenkumar/mdriod/parsers/CourseParser.java
@@ -85,10 +85,16 @@ public class CourseParser {
 				course.setId(jCourseObj.getString(TAG_ID));
 				course.setShortname(jCourseObj.getString(TAG_SHORTNAME));
 				course.setFullname(jCourseObj.getString(TAG_FULLNAME));
-				course.setDatecreated(jCourseObj.getLong(TAG_TIME_CREATED));
-				course.setDatemodified(jCourseObj.getLong(TAG_TIME_MODIFIED));
-				course.setStartdate(jCourseObj.getLong(TAG_START_DATE));
-				course.setSummary(jCourseObj.getString(TAG_SUMMARY));
+				if (jCourseObj.has(TAG_TIME_CREATED))
+					course.setDatecreated(jCourseObj.getLong(TAG_TIME_CREATED));
+				if (jCourseObj.has(TAG_TIME_MODIFIED))
+					course.setDatemodified(jCourseObj
+							.getLong(TAG_TIME_MODIFIED));
+				if (jCourseObj.has(TAG_START_DATE))
+					course.setStartdate(jCourseObj.getLong(TAG_START_DATE));
+				if (jCourseObj.has(TAG_SUMMARY))
+					course.setSummary(jCourseObj.getString(TAG_SUMMARY));
+				
 
 				mCourses.add(course);
 			} catch (JSONException e) {
