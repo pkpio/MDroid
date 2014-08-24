614b26da3125866f3127aabbd708934799cc3396
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-24 23:45:51 +0530

    Deleted custom EditText library
    Will replace the library with in-built setError() method for Android EditText
    
    
    Former-commit-id: ddc0de3a2a26a87f8d301dbec5cd5f38a3b9f4d4

--- libs/customedittext/src/com/andreabaccega/formedittextvalidator/PersonFullNameValidator.java
+++
@@ -1,8 +0,0 @@
-package com.andreabaccega.formedittextvalidator;
-
-public class PersonFullNameValidator extends RegexpValidator {
-	public PersonFullNameValidator(String message) {
-		// will allow people with hyphens in his name or surname. Supports also unicode
-		super(message, "[\\p{L}- ]+");
-	}
-}
--- libs/customedittext/src/com/andreabaccega/formedittextvalidator/PersonNameValidator.java
+++
@@ -1,8 +0,0 @@
-package com.andreabaccega.formedittextvalidator;
-
-public class PersonNameValidator extends RegexpValidator {
-	public PersonNameValidator(String message) {
-		// will allow people with hyphens in his name or surname. Supports also unicode
-		super(message, "[\\p{L}-]+");
-	}
-}
