465b06ef375b7558f7d2e1dcd49d53f4772cb159
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-27 00:20:47 +0530

    Added models: MoodleFunction, MoodleSiteInfo

---
+++ src/in/co/praveenkumar/mdroid/moodlemodel/MoodleFunction.java
@@ -0,0 +1,21 @@
+package in.co.praveenkumar.mdroid.moodlemodel;
+
+public class MoodleFunction {
+	private String name;
+	private String version;
+
+	/**
+	 * function name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * The version number of the component to which the function belongs
+	 */
+	public String getVersion() {
+		return version;
+	}
+
+}
