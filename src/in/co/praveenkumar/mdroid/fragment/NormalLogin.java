6acdce250b05838ba7fe7fbf70ac090de7685852
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-27 22:10:58 +0530

    Replaced logincode in NormalLogin with Login class

--- src/in/co/praveenkumar/mdroid/fragment/NormalLogin.java
+++ src/in/co/praveenkumar/mdroid/fragment/NormalLogin.java
@@ -1,11 +1,8 @@
 package in.co.praveenkumar.mdroid.fragment;
 
 import in.co.praveenkumar.mdroid.apis.R;
-import in.co.praveenkumar.mdroid.moodlemodel.MoodleSiteInfo;
-import in.co.praveenkumar.mdroid.moodlemodel.MoodleToken;
-import in.co.praveenkumar.mdroid.moodlerest.MoodleRestToken;
+import in.co.praveenkumar.mdroid.asynctask.Login;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -23,7 +20,7 @@ public class NormalLogin extends Fragment {
 	EditText passwordET;
 	EditText murlET;
 	Button loginButton;
-	ScrollView loginProgressLL;
+	ScrollView loginProgressSV;
 	TextView loginProgressTV;
 	Context ctx;
 
@@ -53,7 +50,7 @@ public class NormalLogin extends Fragment {
 				.findViewById(R.id.login_normal_password);
 		murlET = (EditText) rootView.findViewById(R.id.login_normal_url);
 		loginButton = (Button) rootView.findViewById(R.id.login_normal_login);
-		loginProgressLL = (ScrollView) rootView
+		loginProgressSV = (ScrollView) rootView
 				.findViewById(R.id.login_progress_layout);
 		loginProgressTV = (TextView) rootView
 				.findViewById(R.id.login_progress_message);
@@ -64,73 +61,8 @@ public class NormalLogin extends Fragment {
 		String password = passwordET.getText().toString();
 		String mUrl = murlET.getText().toString();
 
-		MoodleSiteInfo msf = new MoodleSiteInfo("Praveen");
-		msf.save();
-
-		long i = 2;
-		MoodleSiteInfo site = MoodleSiteInfo.findById(MoodleSiteInfo.class, i);
-		System.out.println("Site info: " + site.getFirstname());
-
-		new asyncNormalLogin(username, password, mUrl).execute("");
-	}
-
-	private class asyncNormalLogin extends AsyncTask<String, Integer, Boolean> {
-		String uname;
-		String pwd;
-		String mUrl;
-		String progress = "";
-
-		public asyncNormalLogin(String uname, String pwd, String mUrl) {
-			this.uname = uname;
-			this.pwd = pwd;
-			this.mUrl = mUrl;
-		}
-
-		@Override
-		protected void onPreExecute() {
-			loginButton.setText("Logging in..");
-			loginButton.setEnabled(false);
-			loginProgressLL.setVisibility(ScrollView.VISIBLE);
-		}
-
-		@Override
-		protected void onProgressUpdate(Integer... params) {
-			loginProgressTV.setText(progress);
-		}
-
-		@Override
-		protected Boolean doInBackground(String... params) {
-			updateProgress("Fetching token");
-			MoodleRestToken mrt = new MoodleRestToken(uname, pwd, mUrl);
-			MoodleToken mt = mrt.getToken();
-
-			if (mt.getToken() == null) {
-				updateProgress("Token fetching failed!");
-				updateProgress("\nError: \n" + mt.getError());
-
-				/*
-				 * -TODO- Set this debugging mode flag as app setting
-				 */
-				if (DEBUGGING_MODE) {
-					updateProgress("Moodle url: " + mt.getReproductionlink());
-					updateProgress("Stacktrace: " + mt.getStacktrace());
-					updateProgress("Debug info: " + mt.getDebuginfo());
-				}
-
-				return null;
-			}
-
-			// Fetch site info
-			updateProgress("Token obtained\n Fetching site info\n");
-
-			return null;
-		}
-
-		private void updateProgress(String status) {
-			progress += status + "\n";
-			publishProgress(0);
-		}
-
+		new Login(username, password, mUrl, loginButton, loginProgressSV,
+				loginProgressTV).execute("");
 	}
 
 }
