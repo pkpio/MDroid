393d482358ba9b568b4439de3f2f8bafe58f3ce6
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-27 22:09:50 +0530

    Added Login class for common login
    Former-commit-id: 2bd187e93656264facf08b57444ce98e3e3fd078

---
+++ src/in/co/praveenkumar/mdroid/asynctask/Login.java
@@ -0,0 +1,158 @@
+package in.co.praveenkumar.mdroid.asynctask;
+
+import in.co.praveenkumar.mdroid.helper.SessionSetting;
+import in.co.praveenkumar.mdroid.moodlemodel.MoodleSiteInfo;
+import in.co.praveenkumar.mdroid.moodlemodel.MoodleToken;
+import in.co.praveenkumar.mdroid.moodlerest.MoodleRestSiteInfo;
+import in.co.praveenkumar.mdroid.moodlerest.MoodleRestToken;
+import android.os.AsyncTask;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class Login extends AsyncTask<String, Integer, Boolean> {
+	String username;
+	String password;
+	String mUrl;
+	String progress = "";
+	String token;
+
+	// Widgets
+	Button loginButton;
+	ScrollView loginProgressSV;
+	TextView loginProgressTV;
+
+	/**
+	 * Do a normal login with Username, Password and Moodle Url.
+	 * 
+	 * @param username
+	 * @param password
+	 * @param mUrl
+	 * @param loginButton
+	 *            Login button widget
+	 * @param loginProgressLL
+	 *            Login progress linearlayout
+	 * @param loginProgressTV
+	 *            Login progress message showing TextView widget
+	 */
+	public Login(String username, String password, String mUrl,
+			Button loginButton, ScrollView loginProgressSV,
+			TextView loginProgressTV) {
+		this.username = username;
+		this.password = password;
+		this.mUrl = mUrl;
+		this.loginButton = loginButton;
+		this.loginProgressSV = loginProgressSV;
+		this.loginProgressTV = loginProgressTV;
+	}
+
+	/**
+	 * Do a Paranoid login with Token and Moodle Url.
+	 * 
+	 * @param token
+	 * @param mUrl
+	 * @param loginButton
+	 *            Login button widget
+	 * @param loginProgressLL
+	 *            Login progress linearlayout
+	 * @param loginProgressTV
+	 *            Login progress message showing TextView widget
+	 */
+	public Login(String token, String mUrl, Button loginButton,
+			ScrollView loginProgressSV, TextView loginProgressTV) {
+		this.token = token;
+		this.mUrl = mUrl;
+		this.loginButton = loginButton;
+		this.loginProgressSV = loginProgressSV;
+		this.loginProgressTV = loginProgressTV;
+	}
+
+	@Override
+	protected void onProgressUpdate(Integer... params) {
+		loginProgressTV.setText(progress);
+	}
+
+	@Override
+	protected void onPreExecute() {
+		loginButton.setText("Logging in..");
+		loginButton.setEnabled(false);
+		loginProgressSV.setVisibility(ScrollView.VISIBLE);
+	}
+
+	@Override
+	protected Boolean doInBackground(String... params) {
+
+		// Get token if required
+		if (token == null)
+			if (!getToken())
+				return null;
+
+		// Get Site info
+		if (!getSiteInfo())
+			return null;
+
+		// Sync data
+		updateProgress("Syncing data\n");
+
+		return null;
+	}
+
+	private void updateProgress(String status) {
+		progress += status + "\n";
+		publishProgress(0);
+	}
+
+	/**
+	 * Get a token from username, password and mUrl. Token found in this.token
+	 * 
+	 * @return True: If token fetched. False: otherwise.
+	 */
+	private Boolean getToken() {
+		updateProgress("Fetching token");
+		MoodleRestToken mrt = new MoodleRestToken(username, password, mUrl);
+		MoodleToken mt = mrt.getToken();
+
+		if (mt.getToken() == null) {
+			updateProgress("Token fetch failed!");
+			updateProgress("\nError: \n" + mt.getError());
+
+			if (SessionSetting.DebugMode) {
+				updateProgress("Moodle url: " + mt.getReproductionlink());
+				updateProgress("Stacktrace: " + mt.getStacktrace());
+				updateProgress("Debug info: " + mt.getDebuginfo());
+			}
+
+			return false;
+		}
+		this.token = mt.getToken();
+
+		return true;
+	}
+
+	/**
+	 * Get siteinfo from this.token and this.mUrl
+	 * 
+	 * @return True: If info fetched. False: otherwise.
+	 */
+	private Boolean getSiteInfo() {
+		updateProgress("Fetching site info");
+		MoodleRestSiteInfo mrsi = new MoodleRestSiteInfo(mUrl, token);
+		MoodleSiteInfo msi = mrsi.getSiteInfo();
+		if (msi.getFullname() == null) {
+			updateProgress("Siteinfo fetch failed!");
+			updateProgress("\nError code: \n" + msi.getErrorcode());
+
+			if (SessionSetting.DebugMode) {
+				updateProgress("Exception: " + msi.getException());
+				updateProgress("Message: " + msi.getMessage());
+				updateProgress("Debug info: " + msi.getDebuginfo());
+			}
+
+			return false;
+		}
+		updateProgress("\nWelcome " + msi.getFullname() + "!\n");
+
+		return true;
+	}
+
+}
