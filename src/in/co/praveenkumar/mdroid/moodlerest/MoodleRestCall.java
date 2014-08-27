5c8ed4812930e189958b0f82f365b6e1e5f5eed7
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-27 20:02:18 +0530

    Added MoodleRestCall class
    Former-commit-id: 886c35c9dc0c2a9d77e22cd13ad7869ae1e0286c

---
+++ src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestCall.java
@@ -0,0 +1,45 @@
+package in.co.praveenkumar.mdroid.moodlerest;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.util.Log;
+
+public class MoodleRestCall {
+	private final String DEBUG_TAG = "MoodleRestCall";
+
+	public InputStreamReader fetchContent(String restUrl, String params) {
+
+		Log.d(DEBUG_TAG, restUrl + params);
+		HttpURLConnection con;
+		try {
+			con = (HttpURLConnection) new URL(restUrl + params)
+					.openConnection();
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Accept", "application/xml");
+			con.setRequestProperty("Content-Language", "en-US");
+			con.setDoOutput(true);
+
+			OutputStreamWriter writer = new OutputStreamWriter(
+					con.getOutputStream());
+			writer.write("");
+			writer.flush();
+			writer.close();
+
+			return new InputStreamReader(con.getInputStream());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+}
