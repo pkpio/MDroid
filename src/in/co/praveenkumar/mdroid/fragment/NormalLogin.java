109126f83e85a990a8a79941c71a6c03c8507407
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-27 17:10:51 +0530

    Login status messages and error reporting added

--- src/in/co/praveenkumar/mdroid/fragment/NormalLogin.java
+++ src/in/co/praveenkumar/mdroid/fragment/NormalLogin.java
@@ -1,13 +1,27 @@
 package in.co.praveenkumar.mdroid.fragment;
 
 import in.co.praveenkumar.mdroid.apis.R;
+import in.co.praveenkumar.mdroid.moodlemodel.MoodleToken;
+import in.co.praveenkumar.mdroid.moodlerest.MoodleRestToken;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class NormalLogin extends Fragment {
+	EditText usernameET;
+	EditText passwordET;
+	EditText murlET;
+	Button loginButton;
+	LinearLayout loginProgressLL;
+	TextView loginProgressTV;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -15,7 +29,99 @@ public class NormalLogin extends Fragment {
 
 		View rootView = inflater.inflate(R.layout.normal_login, container,
 				false);
+		setUpWidgets(rootView);
+
+		loginButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				doNormalLogin();
+			}
+		});
 
 		return rootView;
 	}
+
+	private void setUpWidgets(View rootView) {
+		usernameET = (EditText) rootView
+				.findViewById(R.id.login_normal_username);
+		passwordET = (EditText) rootView
+				.findViewById(R.id.login_normal_password);
+		murlET = (EditText) rootView.findViewById(R.id.login_normal_url);
+		loginButton = (Button) rootView.findViewById(R.id.login_normal_login);
+		loginProgressLL = (LinearLayout) rootView
+				.findViewById(R.id.login_progress_layout);
+		loginProgressTV = (TextView) rootView
+				.findViewById(R.id.login_progress_message);
+	}
+
+	private void doNormalLogin() {
+		String username = usernameET.getText().toString();
+		String password = passwordET.getText().toString();
+		String mUrl = murlET.getText().toString();
+
+		new asyncNormalLogin(username, password, mUrl).execute("");
+	}
+
+	private class asyncNormalLogin extends AsyncTask<String, Integer, Boolean> {
+		String uname;
+		String pwd;
+		String mUrl;
+		String curStatus;
+
+		public asyncNormalLogin(String uname, String pwd, String mUrl) {
+			this.uname = uname;
+			this.pwd = pwd;
+			this.mUrl = mUrl;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			loginButton.setText("Logging in..");
+			loginButton.setEnabled(false);
+			loginProgressLL.setVisibility(LinearLayout.VISIBLE);
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... params) {
+			String progress = loginProgressTV.getText().toString();
+			progress += curStatus + "\n";
+			loginProgressTV.setText(progress);
+		}
+
+		@Override
+		protected Boolean doInBackground(String... params) {
+			updateProgress("Fetching token");
+			MoodleRestToken mrt = new MoodleRestToken(uname, pwd, mUrl);
+			MoodleToken mt = mrt.getToken();
+
+			if (mt.getToken() == null) {
+				updateProgress("Token fetching failed!");
+
+				// Print the errors from moodle
+				if (mt.getError() != null) {
+					updateProgress("\nError: " + mt.getError());
+					updateProgress("\nStacktrace: " + mt.getStacktrace());
+					updateProgress("\nMoodle url: " + mt.getReproductionlink());
+				}
+
+				// Print non-moodle errors
+				else {
+					updateProgress("\nError:\n" + mrt.getErrorsString());
+				}
+				
+				return null;
+			}
+			
+			
+
+			return null;
+		}
+
+		private void updateProgress(String status) {
+			curStatus = status;
+			publishProgress(0);
+		}
+
+	}
+
 }
