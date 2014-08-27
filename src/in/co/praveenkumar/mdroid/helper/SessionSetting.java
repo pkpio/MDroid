6da72b4201f288c0623033ca9c8e44ccf5738f7c
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-27 22:10:23 +0530

    Added SessionSetting class for session params
    Former-commit-id: adcbe0f8f745db066203d6310680ef05a2aae415

---
+++ src/in/co/praveenkumar/mdroid/helper/SessionSetting.java
@@ -0,0 +1,15 @@
+package in.co.praveenkumar.mdroid.helper;
+
+/**
+ * All the session parameters for the current app session are available here.
+ * Params included are token, mUrl, DebugMode etc.,.
+ * 
+ * @author praveen
+ * 
+ */
+public class SessionSetting {
+	public static String token;
+	public static String mUrl;
+	public static Boolean DebugMode = false;
+
+}
