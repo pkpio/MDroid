1d05d0d3a6ac2370b7c5d8fa9f8ff64d4998616a
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-29 02:09:28 +0530

    Added Gson exlude strategy for models with conflicts
    In case of such models, we decode only those fields with serilazedName set by applying this exlusion strategy
    
    
    Former-commit-id: 478ebfef4d7c18549a471ac53685e43b372c00dd

---
+++ src/in/co/praveenkumar/mdroid/helper/GsonExclude.java
@@ -0,0 +1,40 @@
+package in.co.praveenkumar.mdroid.helper;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Why?
+ * Because some of the field names in a model may conflict with those from Sugar class. 
+ * Simply adding a serialize command to those fields will throw exception listed below.
+ * 
+ * So, we add serializedName to all fields in such models and decode only those with 
+ * serializedName using this Gson exclusion strategy. 
+ * 
+ * Exception: http://stackoverflow.com/questions/19315431/gson-tostring-gives-error-illegalargumentexception-multiple-json-fields-name
+ */
+
+/**
+ * This ignores all the fields without a serializeName from decoding
+ * 
+ * @author praveen
+ * 
+ */
+public class GsonExclude implements ExclusionStrategy {
+
+	@Override
+	public boolean shouldSkipClass(Class<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean shouldSkipField(FieldAttributes field) {
+		SerializedName ns = field.getAnnotation(SerializedName.class);
+		if (ns != null)
+			return false;
+		return true;
+	}
+
+}
