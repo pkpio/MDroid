2dd51731117d7385442e82640011a11f8b380d12
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-29 23:02:16 +0530

    Listing of sites in side nav complete
    Former-commit-id: 737a79bb86009cabdd4925808d12d159456e6f46

--- src/in/co/praveenkumar/mdroid/fragment/LeftNavigation.java
+++ src/in/co/praveenkumar/mdroid/fragment/LeftNavigation.java
@@ -1,17 +1,32 @@
 package in.co.praveenkumar.mdroid.fragment;
 
 import in.co.praveenkumar.mdroid.apis.R;
+import in.co.praveenkumar.mdroid.helper.ImageDecoder;
+import in.co.praveenkumar.mdroid.helper.SessionSetting;
+import in.co.praveenkumar.mdroid.moodlemodel.MoodleSiteInfo;
+
+import java.io.File;
+import java.util.List;
+
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class LeftNavigation extends Fragment {
 	ListView navListView;
+	List<MoodleSiteInfo> sites;
+	MoodleSiteInfo currentSite;
+	String[] menuItems = new String[] { "Courses", "Calender", "Forums",
+			"Notes" };
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,14 +36,12 @@ public class LeftNavigation extends Fragment {
 				container, false);
 		navListView = (ListView) rootView.findViewById(R.id.left_nav_list);
 
-		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-				"Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-				"Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-				"OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-				"Android", "iPhone", "WindowsMobile" };
+		// Get sites info
+		SessionSetting session = new SessionSetting(getActivity());
+		currentSite = session.getSiteInfo();
+		sites = MoodleSiteInfo.listAll(MoodleSiteInfo.class);
 
-		final LeftNavListAdapter adapter = new LeftNavListAdapter(
-				getActivity(), values);
+		final LeftNavListAdapter adapter = new LeftNavListAdapter(getActivity());
 		navListView.setAdapter(adapter);
 
 		return rootView;
@@ -36,20 +49,35 @@ public class LeftNavigation extends Fragment {
 
 	public class LeftNavListAdapter extends ArrayAdapter<String> {
 		private final Context context;
-		private final String[] values;
 
-		public LeftNavListAdapter(Context context, String[] values) {
-			super(context, R.layout.list_item_account, values);
+		public LeftNavListAdapter(Context context) {
+			super(context, R.layout.list_item_account, new String[sites.size()]);
 			this.context = context;
-			this.values = values;
 		}
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
+			View rowView = convertView;
 			LayoutInflater inflater = (LayoutInflater) context
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			View rowView = inflater.inflate(R.layout.list_item_account, parent,
+			rowView = inflater.inflate(R.layout.list_item_account, parent,
 					false);
+
+			final TextView userfullnameView = (TextView) rowView
+					.findViewById(R.id.nav_user_fullname);
+			final TextView sitenameView = (TextView) rowView
+					.findViewById(R.id.nav_sitename);
+			final ImageView userimageView = (ImageView) rowView
+					.findViewById(R.id.nav_user_image);
+
+			userfullnameView.setText(sites.get(position).getFullname());
+			sitenameView.setText(sites.get(position).getSitename());
+			Bitmap userImage = ImageDecoder.decodeImage(new File(Environment
+					.getExternalStorageDirectory()
+					+ "/MDroid/."
+					+ sites.get(position).getId()));
+			if (userImage != null)
+				userimageView.setImageBitmap(userImage);
 
 			return rowView;
 		}
