8305886599e9961758388eab7439197f150f7729
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-29 16:32:12 +0530

    Added left navigation fragment

---
+++ src/in/co/praveenkumar/mdroid/fragment/LeftNavigation.java
@@ -0,0 +1,21 @@
+package in.co.praveenkumar.mdroid.fragment;
+
+import in.co.praveenkumar.mdroid.apis.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LeftNavigation extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.left_navigation, container,
+				false);
+		return rootView;
+	}
+
+}
