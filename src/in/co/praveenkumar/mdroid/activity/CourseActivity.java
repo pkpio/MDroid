c06b23f5c4cd5393fbc2c4b52a448f4a526629a9
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-30 02:48:30 +0530

    Added card background layout and listview in course

--- src/in/co/praveenkumar/mdroid/activity/CourseActivity.java
+++ src/in/co/praveenkumar/mdroid/activity/CourseActivity.java
@@ -2,15 +2,73 @@ package in.co.praveenkumar.mdroid.activity;
 
 import in.co.praveenkumar.mdroid.adapter.NavigationDrawer;
 import in.co.praveenkumar.mdroid.apis.R;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class CourseActivity extends NavigationDrawer {
+	CourseListAdapter courseListAdapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_course);
 		setUpDrawer();
+
+		ListView courseList = (ListView) findViewById(R.id.content_course);
+		courseListAdapter = new CourseListAdapter(this);
+		courseList.setAdapter(courseListAdapter);
+	}
+
+	public class CourseListAdapter extends ArrayAdapter<String> {
+		private final Context context;
+
+		public CourseListAdapter(Context context) {
+			super(context, R.layout.list_item_account, new String[10]);
+			this.context = context;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder viewHolder;
+
+			if (convertView == null) {
+				viewHolder = new ViewHolder();
+				LayoutInflater inflater = (LayoutInflater) context
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+				convertView = inflater.inflate(R.layout.list_item_course,
+						parent, false);
+
+				viewHolder.shortname = (TextView) convertView
+						.findViewById(R.id.list_course_shortname);
+				viewHolder.fullname = (TextView) convertView
+						.findViewById(R.id.list_course_fullname);
+
+				// Save the holder with the view
+				convertView.setTag(viewHolder);
+			} else {
+				// Just use the viewHolder and avoid findviewbyid()
+				viewHolder = (ViewHolder) convertView.getTag();
+			}
+
+			// Assign values
+			viewHolder.shortname.setText("CS 101");
+			viewHolder.fullname
+					.setText("Introduction to computer science and programming");
+
+			return convertView;
+		}
+	}
+
+	static class ViewHolder {
+		TextView shortname;
+		TextView fullname;
 	}
 
 }
