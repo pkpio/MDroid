c53d2fae8b0757757363be56bda332bed7db4210
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-29 16:35:43 +0530

    Added Course Activity

--- src/in/co/praveenkumar/mdroid/activity/CourseActivity.java
+++ src/in/co/praveenkumar/mdroid/activity/CourseActivity.java
@@ -1,5 +1,16 @@
 package in.co.praveenkumar.mdroid.activity;
 
-public class CourseActivity {
+import in.co.praveenkumar.mdroid.adapter.NavigationDrawer;
+import in.co.praveenkumar.mdroid.apis.R;
+import android.os.Bundle;
+
+public class CourseActivity extends NavigationDrawer {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_course);
+		setUpDrawer();
+	}
 
 }
