9033b6cce0a8cc573dd45bc5bbeb293555567b11
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-03 19:47:54 +0530

    Calendar activity added
    Former-commit-id: 2ab7d854f1c1ee90c5e2e30358fd9b43e7e40dca

---
+++ src/in/co/praveenkumar/mdroid/activity/CalendarActivity.java
@@ -0,0 +1,17 @@
+package in.co.praveenkumar.mdroid.activity;
+
+import android.os.Bundle;
+import in.co.praveenkumar.mdroid.adapter.NavigationDrawer;
+import in.co.praveenkumar.mdroid.apis.R;
+
+public class CalendarActivity extends NavigationDrawer {
+	
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_course);
+		setUpDrawer();
+	}
+
+}
