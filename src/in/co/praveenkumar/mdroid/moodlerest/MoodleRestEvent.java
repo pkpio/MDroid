773a7b7ea7bfe80331adf4e0a1b7a0e8c66ed607
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-04 19:59:32 +0530

    Increased scope for REST events method

--- src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestEvent.java
+++ src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestEvent.java
@@ -5,6 +5,7 @@ import in.co.praveenkumar.mdroid.moodlemodel.MoodleEvents;
 
 import java.io.Reader;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 
 import android.util.Log;
@@ -27,18 +28,33 @@ public class MoodleRestEvent {
 
 	/**
 	 * Get all Calendar events of given ids in current Moodle site. Allowed list
-	 * of ids are: <br/>
+	 * of ids are:
 	 * <ul>
 	 * <li>courseids (ID_TYPE_COURSE)</li>
 	 * <li>eventids (ID_TYPE_EVENT)</li>
 	 * <li>groupids (ID_TYPE_GROUP)</li>
 	 * </ul>
 	 * 
+	 * @param ids
+	 *            List of ids.
+	 * @param userEvents
+	 *            Set if user events are to be included
+	 * @param siteEvents
+	 *            Set if site events are to be included
+	 * @param idType
+	 *            Type of the ids given. Ex.: use ID_TYPE_COURSE if requesting
+	 *            for course events
+	 * 
+	 * <br/>
+	 *            <b>Note:</b> If only site and user events are required
+	 *            (excluding course or group events), pass null for ids
+	 * 
 	 * @return MoodleEvents
 	 * 
 	 * @author Praveen Kumar Pendyala (praveen@praveenkumar.co.in)
 	 */
-	public MoodleEvents getEventsForIds(List<String> ids, int idType) {
+	public MoodleEvents getEventsForIds(List<String> ids, int idType,
+			Boolean userEvents, Boolean siteEvents) {
 		MoodleEvents mEvents = null;
 		String format = MoodleRestOption.RESPONSE_FORMAT;
 		String function = MoodleRestOption.FUNCTION_GET_EVENTS;
@@ -46,6 +62,23 @@ public class MoodleRestEvent {
 		try {
 			// Adding all parameters.
 			String params = "";
+
+			// Moodle default endtime is today. Setting to year 2200
+			params += "&options[timeend]="
+					+ URLEncoder.encode("7258098600", "UTF-8");
+
+			// Hide user events if set to
+			if (userEvents)
+				params += "&options[userevents]="
+						+ URLEncoder.encode("0", "UTF-8");
+
+			// Hide site events if set to
+			if (siteEvents)
+				params += "&options[siteevents]="
+						+ URLEncoder.encode("0", "UTF-8");
+
+			if (ids == null)
+				ids = new ArrayList<String>();
 
 			for (int i = 0; i < ids.size(); i++) {
 				switch (idType) {
