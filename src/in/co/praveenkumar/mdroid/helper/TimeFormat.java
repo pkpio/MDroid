b1070f7ed14ceb8c6f5c7fab8b464ce8f071ef44
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-06 06:26:32 +0530

    Get relative time method in TimeFormatter

--- src/in/co/praveenkumar/mdroid/helper/TimeFormat.java
+++ src/in/co/praveenkumar/mdroid/helper/TimeFormat.java
@@ -74,6 +74,92 @@ public class TimeFormat {
 	}
 
 	/**
+	 * Evaluates relative time for given time stamp
+	 * 
+	 * @param time
+	 *            in seconds
+	 * @return section title
+	 */
+	public static String getRelTime(int time) {
+		long ltime = ((long) time) * 1000;
+		Calendar c = Calendar.getInstance();
+		int nowMin = c.get(Calendar.MINUTE);
+		int nowHour = c.get(Calendar.HOUR_OF_DAY);
+		int nowDay = c.get(Calendar.DATE);
+		int nowMonth = c.get(Calendar.MONTH);
+		int nowYear = c.get(Calendar.YEAR);
+
+		c.setTimeInMillis(ltime);
+		int givenMin = c.get(Calendar.MINUTE);
+		int givenHour = c.get(Calendar.HOUR_OF_DAY);
+		int givenDay = c.get(Calendar.DATE);
+		int givenMonth = c.get(Calendar.MONTH);
+		int givenYear = c.get(Calendar.YEAR);
+
+		// Past
+		if (time * 1000 < System.currentTimeMillis()) {
+			if (nowYear - givenYear > 1)
+				return (nowYear - givenYear) + " years ago";
+			else if (nowYear - givenYear == 1)
+				return "1 year ago";
+
+			else if (nowMonth - givenMonth > 1)
+				return (nowMonth - givenMonth) + " months ago";
+			else if (nowMonth - givenMonth == 1)
+				return "1 month ago";
+
+			else if (nowDay - givenDay > 1)
+				return (nowDay - givenDay) + " days ago";
+			else if (nowDay - givenDay == 1)
+				return "Yesterday";
+
+			else if (nowHour - givenHour > 1)
+				return (nowHour - givenHour) + " hours ago";
+			else if (nowHour - givenHour == 1)
+				return "1 hour ago";
+
+			else if (nowMin - givenMin > 1)
+				return (nowMin - givenMin) + " minutes ago";
+			else if (nowMin - givenMin == 1)
+				return "1 minute ago";
+
+			else
+				return "A moment ago";
+		}
+
+		// Future
+		else {
+			if (givenYear - nowYear > 1)
+				return "In " + (givenYear - nowYear) + " years";
+			else if (givenYear - nowYear == 1)
+				return "In 1 year";
+
+			else if (givenMonth - nowMonth > 1)
+				return "In " + (givenMonth - nowMonth) + " months";
+			else if (givenMonth - nowMonth == 1)
+				return "In 1 month";
+
+			else if (givenDay - nowDay > 1)
+				return "In " + (givenDay - nowDay) + " days";
+			else if (givenDay - nowDay == 1)
+				return "Tomorrow";
+
+			else if (givenHour - nowHour > 1)
+				return "In " + (givenHour - nowHour) + " hours";
+			else if (givenHour - nowHour == 1)
+				return "In 1 hour";
+
+			else if (givenMin - nowMin > 1)
+				return "In " + (givenMin - nowMin) + " minutes";
+			else if (givenMin - nowMin == 1)
+				return "In 1 minute";
+
+			else
+				return "In a moment";
+		}
+	}
+
+	/**
 	 * Evaluates a simple time format like 12:23 AM
 	 * 
 	 * @param time
