096e897514ed9f3f3eee48d37953618faae45dbb
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-07 01:22:53 +0530

    Calender fragment added
    Former-commit-id: 48609169d5fb72bfd05b375f0e8844a8d4398117

---
+++ src/in/co/praveenkumar/mdroid/fragment/CalenderFragment.java
@@ -0,0 +1,277 @@
+package in.co.praveenkumar.mdroid.fragment;
+
+import in.co.praveenkumar.mdroid.apis.R;
+import in.co.praveenkumar.mdroid.helper.SessionSetting;
+import in.co.praveenkumar.mdroid.helper.TimeFormat;
+import in.co.praveenkumar.mdroid.moodlemodel.MoodleCourse;
+import in.co.praveenkumar.mdroid.moodlemodel.MoodleEvent;
+import in.co.praveenkumar.mdroid.task.EventSyncTask;
+import in.co.praveenkumar.mdroid.view.StickyListView;
+import in.co.praveenkumar.mdroid.view.StickyListView.PinnedSectionListAdapter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class CalenderFragment extends Fragment {
+	Context context;
+	static int courseid = 0;
+	CalendarListAdapter calendarListAdapter;
+	SessionSetting session;
+	List<MoodleEvent> mEvents;
+	ArrayList<CalenderObject> listObjects = new ArrayList<CalenderObject>();
+
+	/**
+	 * Don't use this constructor
+	 */
+	public CalenderFragment() {
+	}
+
+	/**
+	 * If you want to list all events, use courseid = 0
+	 * 
+	 * @param courseid
+	 */
+	public CalenderFragment(int courseid) {
+		CalenderFragment.courseid = courseid;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		this.context = getActivity();
+		View rootView = inflater.inflate(R.layout.frag_calender, container,
+				false);
+		ListView eventList = (ListView) rootView
+				.findViewById(R.id.list_calendar);
+
+		session = new SessionSetting(context);
+		if (courseid == 0)
+			mEvents = MoodleEvent.find(MoodleEvent.class, "siteid = ?",
+					session.getCurrentSiteId() + "");
+		else
+			mEvents = MoodleEvent.find(MoodleEvent.class,
+					"siteid = ? and courseid = ?", session.getCurrentSiteId()
+							+ "", courseid + "");
+		setupCalenderObjects();
+
+		calendarListAdapter = new CalendarListAdapter(context);
+		((StickyListView) eventList).setShadowVisible(false);
+		eventList.setAdapter(calendarListAdapter);
+
+		new ListEventsThread(session.getmUrl(), session.getToken(),
+				session.getCurrentSiteId()).execute("");
+
+		return rootView;
+	}
+
+	private class ListEventsThread extends AsyncTask<String, Integer, Boolean> {
+		Long siteid;
+		EventSyncTask est;
+		Boolean syncStatus;
+
+		public ListEventsThread(String mUrl, String token, Long siteid) {
+			this.siteid = siteid;
+			est = new EventSyncTask(mUrl, token, siteid);
+		}
+
+		@Override
+		protected Boolean doInBackground(String... params) {
+			// Get course ids
+			List<MoodleCourse> mCourses = MoodleCourse.find(MoodleCourse.class,
+					"siteid = ?", siteid + "");
+			ArrayList<String> courseIds = new ArrayList<String>();
+			for (int i = 0; i < mCourses.size(); i++)
+				courseIds.add(mCourses.get(i).getCourseid() + "");
+			syncStatus = est.syncEvents(courseIds);
+
+			if (syncStatus) {
+				mEvents = MoodleEvent.find(MoodleEvent.class, "siteid = ?",
+						session.getCurrentSiteId() + "");
+				setupCalenderObjects();
+				return true;
+			} else
+				return false;
+		}
+
+		@Override
+		protected void onPostExecute(Boolean result) {
+			calendarListAdapter.notifyDataSetChanged();
+		}
+
+	}
+
+	public class CalendarListAdapter extends ArrayAdapter<String> implements
+			PinnedSectionListAdapter {
+		final Context context;
+		static final int TYPE_EVENT = 0;
+		static final int TYPE_DATE = 1;
+		static final int TYPE_COUNT = 2;
+
+		public CalendarListAdapter(Context context) {
+			super(context, R.layout.list_item_event, new String[listObjects
+					.size()]);
+			this.context = context;
+		}
+
+		@Override
+		public int getViewTypeCount() {
+			return TYPE_COUNT;
+		}
+
+		@Override
+		public int getItemViewType(int position) {
+			return listObjects.get(position).viewType;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder viewHolder;
+			int type = getItemViewType(position);
+
+			if (convertView == null) {
+				viewHolder = new ViewHolder();
+				LayoutInflater inflater = (LayoutInflater) context
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+				// Choose layout
+				switch (type) {
+				case TYPE_EVENT:
+					convertView = inflater.inflate(R.layout.list_item_event,
+							parent, false);
+
+					viewHolder.eventname = (TextView) convertView
+							.findViewById(R.id.event_name);
+					viewHolder.eventcourse = (TextView) convertView
+							.findViewById(R.id.event_coursename);
+					viewHolder.eventtime = (TextView) convertView
+							.findViewById(R.id.event_time);
+					viewHolder.eventdesc = (TextView) convertView
+							.findViewById(R.id.event_desc);
+					break;
+				case TYPE_DATE:
+					convertView = inflater.inflate(
+							R.layout.list_item_calender_day, parent, false);
+
+					viewHolder.title = (TextView) convertView
+							.findViewById(R.id.list_calender_time);
+					break;
+				}
+
+				// Save the holder with the view
+				convertView.setTag(viewHolder);
+			} else {
+				viewHolder = (ViewHolder) convertView.getTag();
+			}
+
+			// Assign values
+			switch (type) {
+			case TYPE_EVENT:
+				viewHolder.eventname.setText(listObjects.get(position).event
+						.getName());
+				viewHolder.eventcourse.setText(listObjects.get(position).event
+						.getCoursename());
+				viewHolder.eventtime.setText(TimeFormat
+						.getMinimalTime(listObjects.get(position).event
+								.getTimestart()));
+
+				String description = listObjects.get(position).event
+						.getDescription();
+				if (description == null)
+					description = "";
+				else
+					description = Html.fromHtml(description).toString().trim();
+				viewHolder.eventdesc.setText(description);
+				break;
+			case TYPE_DATE:
+				viewHolder.title.setText(listObjects.get(position).title);
+				break;
+			}
+
+			return convertView;
+		}
+
+		@Override
+		public boolean isItemViewTypePinned(int viewType) {
+			if (viewType == TYPE_DATE)
+				return true;
+			else
+				return false;
+		}
+	}
+
+	static class ViewHolder {
+		TextView eventname;
+		TextView eventcourse;
+		TextView eventtime;
+		TextView eventdesc;
+		TextView title;
+	}
+
+	private void setupCalenderObjects() {
+		if (mEvents == null)
+			return;
+		if (mEvents.size() == 0)
+			return;
+
+		Collections.sort(mEvents, new Comparator<MoodleEvent>() {
+			public int compare(MoodleEvent o1, MoodleEvent o2) {
+				if (o1.getTimestart() == o2.getTimestart())
+					return 0;
+				return o1.getTimestart() < o2.getTimestart() ? -1 : 1;
+			}
+		});
+
+		// Build titles + events objects for pinned listview
+		String titlePrev = TimeFormat.getSection(mEvents.get(0).getTimestart());
+		String titleNow = "";
+		listObjects.add(new CalenderObject(null, CalendarListAdapter.TYPE_DATE,
+				titlePrev));
+		listObjects.add(new CalenderObject(mEvents.get(0),
+				CalendarListAdapter.TYPE_EVENT, titlePrev));
+		for (int i = 1; i < mEvents.size(); i++) {
+			titleNow = TimeFormat.getSection(mEvents.get(i).getTimestart());
+			if (!titleNow.contentEquals(titlePrev))
+				listObjects.add(new CalenderObject(null,
+						CalendarListAdapter.TYPE_DATE, titleNow));
+			listObjects.add(new CalenderObject(mEvents.get(i),
+					CalendarListAdapter.TYPE_EVENT, titleNow));
+			titlePrev = titleNow;
+		}
+	}
+
+	/**
+	 * For simplified pinned listview usage
+	 * 
+	 * @author Praveen Kumar Pendyala (praveen@praveenkumar.co.in)
+	 * 
+	 */
+	class CalenderObject {
+		MoodleEvent event;
+		int viewType;
+		String title;
+
+		public CalenderObject() {
+		}
+
+		public CalenderObject(MoodleEvent event, int viewType, String title) {
+			this.event = event;
+			this.viewType = viewType;
+			this.title = title;
+		}
+	}
+
+}
