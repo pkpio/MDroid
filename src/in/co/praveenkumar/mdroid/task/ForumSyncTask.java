cf51c3cf440a28a73c5db7e26de7f17d94ec273c
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-06 07:04:34 +0530

    ForumSyncTask created
    Former-commit-id: 73756ec2e0e065645119443e3a144246a1b5e19a

---
+++ src/in/co/praveenkumar/mdroid/task/ForumSyncTask.java
@@ -0,0 +1,94 @@
+package in.co.praveenkumar.mdroid.task;
+
+import in.co.praveenkumar.mdroid.moodlemodel.MoodleCourse;
+import in.co.praveenkumar.mdroid.moodlemodel.MoodleForum;
+import in.co.praveenkumar.mdroid.moodlerest.MoodleRestForum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForumSyncTask {
+	String mUrl;
+	String token;
+	long siteid;
+	String error;
+
+	/**
+	 * 
+	 * @param mUrl
+	 * @param token
+	 * @param siteid
+	 * 
+	 * @author Praveen Kumar Pendyala (praveen@praveenkumar.co.in)
+	 */
+	public ForumSyncTask(String mUrl, String token, long siteid) {
+		this.mUrl = mUrl;
+		this.token = token;
+		this.siteid = siteid;
+	}
+
+	/**
+	 * Sync all the events of a course. This will also sync user and site events
+	 * whose scope is outside course.
+	 * 
+	 * @return syncStatus
+	 * 
+	 * @author Praveen Kumar Pendyala (praveen@praveenkumar.co.in)
+	 */
+	public Boolean syncForums(int courseid) {
+		ArrayList<String> courseids = new ArrayList<String>();
+		courseids.add(courseid + "");
+		return syncForums(courseids);
+	}
+
+	/**
+	 * Sync all the forums in the list of courses.
+	 * 
+	 * @return syncStatus
+	 * 
+	 * @author Praveen Kumar Pendyala (praveen@praveenkumar.co.in)
+	 */
+	public Boolean syncForums(ArrayList<String> courseids) {
+		MoodleRestForum mrf = new MoodleRestForum(mUrl, token);
+		ArrayList<MoodleForum> mForums = mrf.getForums(courseids);
+
+		/** Error checking **/
+		// Some network or encoding issue.
+		if (mForums == null) {
+			error = "Network issue!";
+			return false;
+		}
+
+		// Moodle exception
+		if (mForums.size() == 0) {
+			error = "No data received";
+			// No additional debug info as that needs context
+			return false;
+		}
+
+		List<MoodleForum> dbForums;
+		List<MoodleCourse> dbCourses;
+		MoodleForum forum = new MoodleForum();
+		for (int i = 0; i < mForums.size(); i++) {
+			forum = mForums.get(i);
+			forum.setSiteid(siteid);
+			/*
+			 * -TODO- Improve this search with only Sql operation
+			 */
+			dbForums = MoodleForum.find(MoodleForum.class,
+					"forumid = ? and siteid = ?", forum.getForumid() + "",
+					siteid + "");
+			dbCourses = MoodleCourse.find(MoodleCourse.class,
+					"courseid = ? and siteid = ?", forum.getCourseid() + "",
+					siteid + "");
+			if (dbCourses.size() > 0)
+				forum.setCoursename(dbCourses.get(0).getShortname());
+			if (dbForums.size() > 0)
+				forum.setId(dbForums.get(0).getId());
+			forum.save();
+		}
+
+		return true;
+	}
+
+}
