92c07ea267864ec5eaf56417bf6333e01b16a607
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-07 01:26:10 +0530

    All fragments updated with a no param constructor
    Former-commit-id: 864139e76044a54aa14984dc14935c7b8397190e

--- src/in/co/praveenkumar/mdroid/fragment/CourseFragment.java
+++ src/in/co/praveenkumar/mdroid/fragment/CourseFragment.java
@@ -7,10 +7,10 @@ import in.co.praveenkumar.mdroid.moodlemodel.MoodleCourse;
 
 import java.util.List;
 
-import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
