6f60fa343c3f1286ae4372f4b5b0950ebd75c789
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-28 17:47:52 +0530

    Discussion activity added

---
+++ src/in/co/praveenkumar/mdroid/activity/DiscussionActivity.java
@@ -0,0 +1,20 @@
+package in.co.praveenkumar.mdroid.activity;
+
+import android.os.Bundle;
+import in.co.praveenkumar.mdroid.apis.R;
+import in.co.praveenkumar.mdroid.helper.AppNavigationDrawer;
+
+public class DiscussionActivity extends AppNavigationDrawer {
+	int forumid;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_discussion);
+		setUpDrawer();
+	}
+	
+	public int getForumid() {
+        return forumid;
+    }
+}
