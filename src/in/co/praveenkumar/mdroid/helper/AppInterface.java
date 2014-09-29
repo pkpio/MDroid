bf2c2f415a7617b746ee02b095414a83e6203ed5
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-09-29 16:01:13 +0530

    Added interface for forumid passing between fragments and activities

--- src/in/co/praveenkumar/mdroid/helper/AppInterface.java
+++ src/in/co/praveenkumar/mdroid/helper/AppInterface.java
@@ -1,6 +1,13 @@
 package in.co.praveenkumar.mdroid.helper;
 
 public class AppInterface {
+
+	/**
+	 * Gives an interface for changing the state of the app drawers
+	 * 
+	 * @author Praveen Kumar Pendyala (praveen@praveenkumar.co.in)
+	 * 
+	 */
 	public interface DrawerStateInterface {
 		/**
 		 * Set navigation drawers state
@@ -9,6 +16,25 @@ public class AppInterface {
 		 *            True: open. False: close
 		 */
 		public void setDrawerState(Boolean state);
+	}
+
+	/**
+	 * Gives an interface for passing forumid between activity and fragments.
+	 * 
+	 * Useful in Discussion fragment and discussion activity
+	 * 
+	 * @author Praveen Kumar Pendyala (praveen@praveenkumar.co.in)
+	 * 
+	 */
+	public interface ForumIdInterface {
+		/**
+		 * Set navigation drawers state
+		 * 
+		 * @param state
+		 *            True: open. False: close
+		 */
+		public int getForumId();
+
 	}
 
 }
