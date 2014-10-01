e4c3b4afaa477a57db52f3e8b32793b65d872c8b
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-01 21:14:59 +0530

    MDroid model feature added
    Former-commit-id: 21f1145cf0dfc936506b92a357934572c8a14f2c

---
+++ src/in/co/praveenkumar/mdroid/donation/MDroidModelFeature.java
@@ -0,0 +1,95 @@
+package in.co.praveenkumar.mdroid.donation;
+
+import com.google.gson.annotations.SerializedName;
+import com.orm.SugarRecord;
+import com.orm.dsl.Ignore;
+
+public class MDroidModelFeature extends SugarRecord<MDroidModelFeature> {
+	@Ignore
+	public static final int NOT_IMPLEMENTED = 0;
+
+	@Ignore
+	public static final int PARTIALLY_IMPLEMENTED = 1;
+
+	@Ignore
+	public static final int IMPLEMENTED = 2;
+
+	@SerializedName("featureid")
+	int featureid;
+
+	@SerializedName("name")
+	String name;
+
+	@SerializedName("status")
+	int status;
+
+	@SerializedName("votestarget")
+	int votestarget;
+
+	@SerializedName("votescasted")
+	int votescasted;
+
+	@SerializedName("description")
+	String description;
+
+	/**
+	 * Get id given to the feature. This has to be mapped with buying ids for
+	 * proper vote mapping.
+	 * 
+	 * @return featureid
+	 */
+	public int getFeatureid() {
+		return featureid;
+	}
+
+	/**
+	 * Get feature name or title
+	 * 
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Get the current implementation status of the feature. Available status
+	 * are:<br/>
+	 * NOT_IMPLEMENTED<br/>
+	 * PARTIALLY_IMPLEMENTED<br/>
+	 * IMPLEMENTED
+	 * 
+	 * @return status
+	 */
+	public int getStatus() {
+		return status;
+	}
+
+	/**
+	 * Get the set target number of votes for this feature.
+	 * 
+	 * @return votestarget
+	 */
+	public int getVotestarget() {
+		return votestarget;
+	}
+
+	/**
+	 * Votes casted so far. Not realtime. Updated periodically by developer
+	 * manually.
+	 * 
+	 * @return votescasted
+	 */
+	public int getVotescasted() {
+		return votescasted;
+	}
+
+	/**
+	 * Description of the feature
+	 * 
+	 * @return description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+}
