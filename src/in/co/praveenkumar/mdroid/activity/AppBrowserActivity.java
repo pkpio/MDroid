5d979517bae3219300bd33790fe2b5e79cf5cace
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-03 00:55:57 +0530

    Form filling attempted in AppBrowser

--- src/in/co/praveenkumar/mdroid/activity/AppBrowserActivity.java
+++ src/in/co/praveenkumar/mdroid/activity/AppBrowserActivity.java
@@ -1,9 +1,13 @@
 package in.co.praveenkumar.mdroid.activity;
 
-import in.co.praveenkumar.mdroid.legacy.R;
 import in.co.praveenkumar.mdroid.helper.AppNavigationDrawer;
+import in.co.praveenkumar.mdroid.legacy.R;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.CookieSyncManager;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -21,11 +25,35 @@ public class AppBrowserActivity extends AppNavigationDrawer {
 		CookieSyncManager.createInstance(this);
 		CookieSyncManager.getInstance().startSync();
 
+		mBrowser.getSettings().setJavaScriptEnabled(true);
+		mBrowser.getSettings().setDomStorageEnabled(true);
+		mBrowser.setWebChromeClient(new WebChromeClient() {
+			@Override
+			public boolean onJsAlert(WebView view, String url, String message,
+					JsResult result) {
+				return super.onJsAlert(view, url, message, result);
+			}
+		});
+
 		mBrowser.setWebViewClient(new WebViewClient() {
 			@Override
 			public boolean shouldOverrideUrlLoading(WebView view, String url) {
 				view.loadUrl(url);
+				// Login user if not logged in
+				// if (view.getUrl() == "") {
+
+				// }
 				return true;
+			}
+
+			int count = 0;
+
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				view.loadUrl("javascript:"
+						+ "document.getElementById('password').value='lola';");
+				count++;
+				System.out.println("Page loaded!");
 			}
 		});
 
