a6aab5674c0d2e639910d9097a2093a6324e44ee
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-06 11:58:40 +0530

    Added ViewHolder class to hold all login Progress views

---
+++ src/in/co/praveenkumar/mdroid/view/LoginStatusViewHolder.java
@@ -0,0 +1,15 @@
+package in.co.praveenkumar.mdroid.view;
+
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class LoginStatusViewHolder {
+	public Button loginButton;
+	public RelativeLayout statusLayout;
+	public TextView progressTitle;
+	public ProgressBar progressBar;
+	public TextView progressText;
+	public Button retryButton;
+}
