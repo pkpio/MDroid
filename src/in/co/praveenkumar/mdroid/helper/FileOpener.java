79c1eab347f6e0a1ff087e7c0ad875326f7bd111
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-07 13:40:12 +0530

    FileOpener class added in helper
    Former-commit-id: 44a2911402365b3e872459c9c10c14c33a20538b

---
+++ src/in/co/praveenkumar/mdroid/helper/FileOpener.java
@@ -0,0 +1,49 @@
+package in.co.praveenkumar.mdroid.helper;
+
+import java.io.File;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
+
+public class FileOpener {
+
+	/**
+	 * Opens a file. Shows a toast when opening failed.
+	 * 
+	 * @param context
+	 * @param file
+	 */
+	public static void open(Context context, String file) {
+		open(context, new File(file));
+	}
+
+	/**
+	 * Opens a file. Shows a toast when opening failed.
+	 * 
+	 * @param context
+	 * @param file
+	 */
+	public static void open(Context context, File file) {
+		MimeTypeMap myMime = MimeTypeMap.getSingleton();
+		String extension = MimeTypeMap.getFileExtensionFromUrl(file.toString());
+		String mimeType = myMime.getMimeTypeFromExtension(extension);
+
+		// Setup intent
+		Intent newIntent = new Intent(Intent.ACTION_VIEW);
+		newIntent.setDataAndType(Uri.fromFile(file), mimeType);
+		newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+		// Open file
+		try {
+			context.startActivity(newIntent);
+		} catch (android.content.ActivityNotFoundException e) {
+			Toast.makeText(context,
+					"No application to open file type : " + extension,
+					Toast.LENGTH_LONG).show();
+		}
+	}
+
+}
