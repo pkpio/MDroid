20292f0d13d5671fa5c629654c5cb6cddd2146f0
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-08 19:58:51 +0530

    MessageDialog messaging sending complete. Working.
    Former-commit-id: a368d226b8c64bfb475d2015a0cbf2e8c07c8a0e

--- src/in/co/praveenkumar/mdroid/dialog/MessageDialog.java
+++ src/in/co/praveenkumar/mdroid/dialog/MessageDialog.java
@@ -1,18 +1,27 @@
 package in.co.praveenkumar.mdroid.dialog;
 
 import in.co.praveenkumar.mdroid.helper.LetterColor;
+import in.co.praveenkumar.mdroid.helper.SessionSetting;
 import in.co.praveenkumar.mdroid.legacy.R;
 import in.co.praveenkumar.mdroid.moodlemodel.MoodleContact;
+import in.co.praveenkumar.mdroid.moodlerest.MoodleRestMessage;
 import android.app.Dialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MessageDialog extends Dialog {
+public class MessageDialog extends Dialog implements
+		android.view.View.OnClickListener {
 	Context context;
 	MoodleContact contact = new MoodleContact();
+	EditText messageET;
 
 	public MessageDialog(Context context) {
 		super(context);
@@ -30,6 +39,8 @@ public class MessageDialog extends Dialog {
 		TextView userName = (TextView) findViewById(R.id.dialog_contact_name);
 		LinearLayout headerLine = (LinearLayout) findViewById(R.id.dialog_header_line);
 		TextView unreadCount = (TextView) findViewById(R.id.dialog_unread_count);
+		ImageView sendButton = (ImageView) findViewById(R.id.dialog_message_sendbutton);
+		messageET = (EditText) findViewById(R.id.dialog_message_text);
 
 		// Set values
 		char letter = (contact.getFullname().length() > 0) ? contact
@@ -42,10 +53,61 @@ public class MessageDialog extends Dialog {
 			unreadCount.setText(contact.getUnread() + "");
 		else
 			unreadCount.setVisibility(TextView.GONE);
+
+		// Send button listener
+		sendButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.dialog_message_sendbutton:
+			SessionSetting session = new SessionSetting(context);
+			new AsyncMessageSender(session.getmUrl(), session.getToken(),
+					contact.getContactid(), messageET.getText().toString())
+					.execute("");
+			break;
+		}
+		dismiss();
 	}
 
 	public void setContact(MoodleContact contact) {
 		this.contact = contact;
+	}
+
+	private class AsyncMessageSender extends
+			AsyncTask<String, Integer, Boolean> {
+		String mUrl;
+		String token;
+		int userid;
+		String message;
+		MoodleRestMessage mrm;
+
+		public AsyncMessageSender(String mUrl, String token, int userid,
+				String message) {
+			this.mUrl = mUrl;
+			this.token = token;
+			this.userid = userid;
+			this.message = message;
+		}
+
+		@Override
+		protected Boolean doInBackground(String... params) {
+			mrm = new MoodleRestMessage(mUrl, token);
+			return mrm.sendMessage(userid, message);
+		}
+
+		@Override
+		protected void onPostExecute(Boolean result) {
+			if (!result)
+				Toast.makeText(context,
+						"Message sending failed. Error: " + mrm.getError(),
+						Toast.LENGTH_LONG).show();
+			else
+				Toast.makeText(context, "Message sent!", Toast.LENGTH_SHORT)
+						.show();
+		}
+
 	}
 
 }
