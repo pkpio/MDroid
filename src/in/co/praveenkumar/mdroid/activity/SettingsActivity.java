6777d07f7b0fe436a6975b5f9bc3085aa3d87a1e
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-09 16:42:58 +0530

    Settings activity added

---
+++ src/in/co/praveenkumar/mdroid/activity/SettingsActivity.java
@@ -0,0 +1,48 @@
+package in.co.praveenkumar.mdroid.activity;
+
+import in.co.praveenkumar.mdroid.legacy.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity implements
+		OnPreferenceClickListener {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setTitle("Settings");
+		addPreferencesFromResource(R.xml.preferences);
+
+		// Add preference click listeners
+		findPreference("logout").setOnPreferenceClickListener(this);
+		findPreference("resetCourseData").setOnPreferenceClickListener(this);
+		findPreference("resetForumData").setOnPreferenceClickListener(this);
+		findPreference("resetCalendarData").setOnPreferenceClickListener(this);
+
+		findPreference("help").setOnPreferenceClickListener(this);
+		findPreference("privacyPolicy").setOnPreferenceClickListener(this);
+
+		findPreference("aboutMDroid").setOnPreferenceClickListener(this);
+		findPreference("aboutDev").setOnPreferenceClickListener(this);
+		findPreference("licenses").setOnPreferenceClickListener(this);
+	}
+
+	@Override
+	public boolean onPreferenceClick(Preference preference) {
+		String key = preference.getKey();
+
+		if (key.contentEquals("licenses")) {
+			Intent i = new Intent(this, AppBrowserActivity.class);
+			i.putExtra("url", "file:///android_asset/os_licenses.html");
+			this.startActivity(i);
+		}
+
+		System.out.println(preference.getKey());
+		return false;
+	}
+
+}
