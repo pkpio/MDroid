91ca69c6ea71d6e98c5748861777061846a41a50
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-11 21:04:41 +0530

    WebservicesOff activity implemented
    Former-commit-id: 649242c06e3b961b7676f117b925b473da5a10e8

---
+++ src/in/co/praveenkumar/mdroid/activity/WebservicesoffActivity.java
@@ -0,0 +1,32 @@
+package in.co.praveenkumar.mdroid.activity;
+
+import in.co.praveenkumar.mdroid.legacy.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class WebservicesoffActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_webservices_off);
+	}
+
+	public void tryLegacyVersion(View v) {
+		Intent i = new Intent(
+				Intent.ACTION_VIEW,
+				Uri.parse("market://details?id=in.co.praveenkumar.mdroid.legacy"));
+		startActivity(i);
+	}
+
+	public void webservicesHelp(View v) {
+		Intent i = new Intent(
+				Intent.ACTION_VIEW,
+				Uri.parse("http://mdroid.praveenkumar.co.in/#!simple-webservices-setup.md"));
+		startActivity(i);
+	}
+
+}
