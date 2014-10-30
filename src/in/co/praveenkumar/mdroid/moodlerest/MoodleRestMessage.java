4a4ad64c2bd8cfa1a4ba5d24de97ffb9437021f5
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-10-31 00:16:11 +0530

    MoodleRestMessage updated with fewer methods using MoodleMessage

--- src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestMessage.java
+++ src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestMessage.java
@@ -14,11 +14,6 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 public class MoodleRestMessage {
-	public static final int MESSAGE_FORMAT_HTML = 1;
-	public static final int MESSAGE_FORMAT_MOODLE = 0;
-	public static final int MESSAGE_FORMAT_PLAIN = 2;
-	public static final int MESSAGE_FORMAT_MARKDOWN = 4;
-
 	private final String DEBUG_TAG = "MoodleRestMessage";
 	private String mUrl;
 	private String token;
@@ -29,56 +24,16 @@ public class MoodleRestMessage {
 		this.token = token;
 	}
 
-	// -TODO- Remove so many methods for sendMessage and use MoodleMessage as
-	// only param and just one method
-
-	/**
-	 * Sends a message to a Moodle user. Uses plaintext as the message format.
-	 * 
-	 * @param userid
-	 *            Moodle userid of the user
-	 * @param message
-	 *            message content
-	 * 
-	 * @return status - true if success. false if failed.
-	 */
-	public Boolean sendMessage(int userid, String message) {
-		return sendMessage(userid, message, MESSAGE_FORMAT_PLAIN);
-	}
-
 	/**
 	 * Sends a message to a Moodle user.
 	 * 
-	 * @param userid
-	 *            Moodle userid of the user
 	 * @param message
-	 *            message content
-	 * @param messageformat
-	 *            message format. Use contants MESSAGE_FORMAT_
+	 *            Message to send with the required params (touserid, text at
+	 *            the least) set
 	 * 
 	 * @return status - true if success. false if failed.
 	 */
-	public Boolean sendMessage(int userid, String message, int messageformat) {
-		return sendMessage(userid, message, MESSAGE_FORMAT_PLAIN, "");
-	}
-
-	/**
-	 * Sends a message to a Moodle user.
-	 * 
-	 * @param userid
-	 *            Moodle userid of the user
-	 * @param message
-	 *            message content
-	 * @param messageformat
-	 *            message format. Use contants MESSAGE_FORMAT_
-	 * @param clientmsgid
-	 *            your own client id for the message. If this id is provided,
-	 *            the fail message id will be returned to you
-	 * 
-	 * @return status - true if success. false if failed.
-	 */
-	public Boolean sendMessage(int userid, String message, int messageformat,
-			String clientmsgid) {
+	public Boolean sendMessage(MoodleMessage message) {
 		String format = MoodleRestOption.RESPONSE_FORMAT;
 		String function = MoodleRestOption.FUNCTION_SEND_MESSAGE;
 		ArrayList<MoodleMessage> mMessages = new ArrayList<MoodleMessage>();
@@ -88,14 +43,16 @@ public class MoodleRestMessage {
 			String params = "" + URLEncoder.encode("", "UTF-8");
 
 			params += "&messages[0][touserid]="
-					+ URLEncoder.encode(String.valueOf(userid), "UTF-8");
+					+ URLEncoder.encode(String.valueOf(message.getTouserid()),
+							"UTF-8");
 			params += "&messages[0][text]="
-					+ URLEncoder.encode(message, "UTF-8");
+					+ URLEncoder.encode(message.getText(), "UTF-8");
 			params += "&messages[0][textformat]="
-					+ URLEncoder.encode(String.valueOf(messageformat), "UTF-8");
-			if (!clientmsgid.contentEquals(""))
+					+ URLEncoder.encode(
+							String.valueOf(message.getTextformat()), "UTF-8");
+			if (message.getClientmsgid() != null)
 				params += "&messages[0][clientmsgid]="
-						+ URLEncoder.encode(clientmsgid, "UTF-8");
+						+ URLEncoder.encode(message.getClientmsgid(), "UTF-8");
 
 			// Build a REST call url to make a call.
 			String restUrl = mUrl + "/webservice/rest/server.php" + "?wstoken="
