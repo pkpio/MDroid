290d230ed45c266b23f8e2c0b5a882ff89b2e716
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-11-04 21:23:45 +0100

    Messaging activity updated with initial setups
    Former-commit-id: aad95736abe2f3b436d98351e8e9e2bd66637021

--- src/in/co/praveenkumar/mdroid/activity/MessagingActivity.java
+++ src/in/co/praveenkumar/mdroid/activity/MessagingActivity.java
@@ -1,5 +1,17 @@
 package in.co.praveenkumar.mdroid.activity;
 
-public class MessagingActivity {
+import in.co.praveenkumar.R;
+import in.co.praveenkumar.mdroid.helper.AppNavigationDrawer;
+import android.os.Bundle;
+
+public class MessagingActivity extends AppNavigationDrawer {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_messaging);
+		setUpDrawer();
+		setTitle("Messaging");
+	}
 
 }
