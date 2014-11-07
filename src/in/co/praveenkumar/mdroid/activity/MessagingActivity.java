ceb05447e967e2729d68309c26e3a74ade21391a
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-11-07 09:48:24 +0100

    Implemented useridinterface in Messaging activity

--- src/in/co/praveenkumar/mdroid/activity/MessagingActivity.java
+++ src/in/co/praveenkumar/mdroid/activity/MessagingActivity.java
@@ -1,10 +1,13 @@
 package in.co.praveenkumar.mdroid.activity;
 
 import in.co.praveenkumar.R;
+import in.co.praveenkumar.mdroid.helper.AppInterface.UserIdInterface;
 import in.co.praveenkumar.mdroid.helper.AppNavigationDrawer;
 import android.os.Bundle;
 
-public class MessagingActivity extends AppNavigationDrawer {
+public class MessagingActivity extends AppNavigationDrawer implements
+		UserIdInterface {
+	int userid;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,16 @@ public class MessagingActivity extends AppNavigationDrawer {
 		setUpDrawer();
 		getSupportActionBar().setTitle("Messaging");
 		getSupportActionBar().setIcon(R.drawable.icon_message);
+	}
+
+	@Override
+	public int getUserId() {
+		return this.userid;
+	}
+
+	@Override
+	public void setUserId(int userid) {
+		this.userid = userid;
 	}
 
 }
