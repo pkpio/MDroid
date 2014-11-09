8ff5b82cf76634b7264ba38ebfd34c2aa7cdc691
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-11-09 21:33:30 +0100

    Param class added

---
+++ src/in/co/praveenkumar/mdroid/helper/Param.java
@@ -0,0 +1,12 @@
+package in.co.praveenkumar.mdroid.helper;
+
+/**
+ * Hold all the common params for the application
+ * 
+ * @author Praveen Kumar Pendyala <praveen@praveenkumar.co.in>
+ * 
+ */
+public class Param {
+	public static final String DefaultMessageSignature = "Sent from MDroid - Moodle for Android";
+
+}
