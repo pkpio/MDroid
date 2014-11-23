d78ba7e4954decb84dc458ddd805abefffc3712b
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-11-23 16:33:32 +0100

    Swipe-to-refresh implemented in Contacts
    Former-commit-id: 55de82635c41aa467ab11661497d9b421feabb89

--- src/in/co/praveenkumar/mdroid/fragment/ContactFragment.java
+++ src/in/co/praveenkumar/mdroid/fragment/ContactFragment.java
@@ -13,33 +13,37 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class ContactFragment extends Fragment {
+public class ContactFragment extends Fragment implements OnRefreshListener {
 	final String DEBUG_TAG = "ContactsFragment";
 	List<MoodleContact> contacts;
 	ContactListAdapter adapter;
 	SessionSetting session;
 	LinearLayout chatEmptyLayout;
+	SwipeRefreshLayout swipeLayout;
+	ListView contactList;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		View rootView = inflater.inflate(R.layout.frag_right_navigation,
-				container, false);
+		View rootView = inflater.inflate(R.layout.frag_contact, container,
+				false);
 		chatEmptyLayout = (LinearLayout) rootView
 				.findViewById(R.id.chat_empty_layout);
-		ListView navListView = (ListView) rootView
-				.findViewById(R.id.right_nav_list);
+		contactList = (ListView) rootView.findViewById(R.id.right_nav_list);
 
 		// Get sites info
 		session = new SessionSetting(getActivity());
@@ -47,10 +51,10 @@ public class ContactFragment extends Fragment {
 				session.getCurrentSiteId() + "");
 
 		adapter = new ContactListAdapter(getActivity());
-		navListView.setAdapter(adapter);
+		contactList.setAdapter(adapter);
 
 		// OnItem click
-		navListView.setOnItemClickListener(new OnItemClickListener() {
+		contactList.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1,
 					int position, long arg3) {
@@ -60,6 +64,10 @@ public class ContactFragment extends Fragment {
 				md.show();
 			}
 		});
+
+		swipeLayout = (SwipeRefreshLayout) rootView
+				.findViewById(R.id.swipe_refresh);
+		setupSwipeLayout();
 
 		new contactSyncerBg().execute("");
 
@@ -166,6 +174,11 @@ public class ContactFragment extends Fragment {
 	private class contactSyncerBg extends AsyncTask<String, Integer, Boolean> {
 
 		@Override
+		protected void onPreExecute() {
+			swipeLayout.setRefreshing(true);
+		}
+
+		@Override
 		protected Boolean doInBackground(String... params) {
 			ContactSyncTask cst = new ContactSyncTask(session.getmUrl(),
 					session.getToken(), session.getCurrentSiteId());
@@ -182,8 +195,41 @@ public class ContactFragment extends Fragment {
 			adapter.notifyDataSetChanged();
 			if (contacts.size() != 0)
 				chatEmptyLayout.setVisibility(LinearLayout.GONE);
+			swipeLayout.setRefreshing(false);
 		}
 
+	}
+
+	@Override
+	public void onRefresh() {
+		new contactSyncerBg().execute("");
+	}
+
+	void setupSwipeLayout() {
+		if (swipeLayout == null || contactList == null)
+			return;
+
+		swipeLayout.setColorSchemeResources(R.color.refresh_green,
+				R.color.refresh_red, R.color.refresh_blue,
+				R.color.refresh_yellow);
+		swipeLayout.setOnRefreshListener(this);
+
+		// Link swipeLayout with underlying listview
+		contactList.setOnScrollListener(new AbsListView.OnScrollListener() {
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+			}
+
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
+				int topRowVerticalPosition = (contactList == null || contactList
+						.getChildCount() == 0) ? 0 : contactList.getChildAt(0)
+						.getTop();
+				swipeLayout.setEnabled(topRowVerticalPosition >= 0);
+			}
+		});
 	}
 
 }
