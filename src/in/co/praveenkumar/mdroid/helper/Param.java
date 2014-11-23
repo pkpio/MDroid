b9c69a119b411200c78e80ca46b0214d4cfb34b5
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-11-23 15:48:11 +0100

    Added new billing params

--- src/in/co/praveenkumar/mdroid/helper/Param.java
+++ src/in/co/praveenkumar/mdroid/helper/Param.java
@@ -10,7 +10,11 @@ public class Param {
 	public static final String DEFAULT_MSG_SIGN = "Sent from MDroid";
 
 	// Billing related
-	public static final String BILLING_DONATION_PRODUCT_ID = "";
-	public static final String BILLING_LICENSE_KEY = "";
+	public static final String BILLING_DONATION_PRODUCT_ID = "in.co.praveenkumar.mdroid.donate";
+	public static final String BILLING_FEATURE_NOTIFICATIONS_PRODUCT_ID = "in.co.praveenkumar.mdroid.donate.features.new_content_notifications";
+	public static final String BILLING_FEATURE_SEARCH_PRODUCT_ID = "in.co.praveenkumar.mdroid.donate.features.search_option";
+	public static final String BILLING_FEATURE_PARTICIPANTS_PRODUCT_ID = "in.co.praveenkumar.mdroid.donate.features.course_participants";
+	public static final String BILLING_FEATURE_UPLOADS_PRODUCT_ID = "in.co.praveenkumar.mdroid.donate.features.uploading_files";
+	public static final String BILLING_LICENSE_KEY = "02425045829981832447";
 
 }
