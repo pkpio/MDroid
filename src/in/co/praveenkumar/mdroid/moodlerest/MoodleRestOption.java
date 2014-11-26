795be94eed0924dc7f848972d0df5a6cc78a4f08
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-11-26 14:55:24 +0100

    False fix for contents compatibility. Reverting.
    Former-commit-id: 1719546f0270cece87dbdc110eca855170d24cbd

--- src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestOption.java
+++ src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestOption.java
@@ -17,7 +17,7 @@ public class MoodleRestOption {
 	// Function names for getting contents
 	public static final String FUNCTION_GET_ALL_COURSES = "moodle_course_get_courses";// core_course_get_courses
 	public static final String FUNCTION_GET_ENROLLED_COURSES = "moodle_enrol_get_users_courses";//core_enrol_get_users_courses
-	public static final String FUNCTION_GET_COURSE_CONTENTS = "moodle_course_get_courses";// "core_course_get_contents";
+	public static final String FUNCTION_GET_COURSE_CONTENTS = "core_course_get_contents";// "core_course_get_contents";
 	public static final String FUNCTION_GET_FORUMS = "mod_forum_get_forums_by_courses";
 	public static final String FUNCTION_GET_DISCUSSIONS = "mod_forum_get_forum_discussions";
 	public static final String FUNCTION_GET_POSTS = "mod_forum_get_forum_discussion_posts";
