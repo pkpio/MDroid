b0e4030bb12563c651a6e9021b62bfd78d2dbe58
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-11-27 23:00:44 +0100

    MoodleRestOptions updated - Messages compatibility improved and .. User profiles from course function added

--- src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestOption.java
+++ src/in/co/praveenkumar/mdroid/moodlerest/MoodleRestOption.java
@@ -17,15 +17,15 @@ public class MoodleRestOption {
 	// Function names for getting contents
 	public static final String FUNCTION_GET_ALL_COURSES = "moodle_course_get_courses";// core_course_get_courses
 	public static final String FUNCTION_GET_ENROLLED_COURSES = "moodle_enrol_get_users_courses";// core_enrol_get_users_courses
-	public static final String FUNCTION_GET_COURSE_CONTENTS = "core_course_get_contents";// "core_course_get_contents";
+	public static final String FUNCTION_GET_COURSE_CONTENTS = "core_course_get_contents";// no_alternatives
 	public static final String FUNCTION_GET_FORUMS = "mod_forum_get_forums_by_courses";
 	public static final String FUNCTION_GET_DISCUSSIONS = "mod_forum_get_forum_discussions";
 	public static final String FUNCTION_GET_POSTS = "mod_forum_get_forum_discussion_posts";
-	public static final String FUNCTION_GET_SITE_INFO = "moodle_webservice_get_siteinfo";
-	public static final String FUNCTION_GET_CONTACTS = "core_message_get_contacts";
-	public static final String FUNCTION_GET_EVENTS = "core_calendar_get_calendar_events";
-	public static final String FUNCTION_GET_USERS_FROM_FIELD = "core_user_get_users_by_field";
-	public static final String FUNCTION_SEND_MESSAGE = "core_message_send_instant_messages";
+	public static final String FUNCTION_GET_SITE_INFO = "moodle_webservice_get_siteinfo";// core_webservice_get_site_info()
+	public static final String FUNCTION_GET_CONTACTS = "core_message_get_contacts";// no_alternatives
+	public static final String FUNCTION_GET_EVENTS = "core_calendar_get_calendar_events";// no_alternatives
+	public static final String FUNCTION_SEND_MESSAGE = "moodle_message_send_instantmessages";// core_message_send_instant_messages
 	public static final String FUNCTION_GET_MESSAGES = "local_mobile_core_message_get_messages";
+	public static final String FUNCTION_GET_USERS_FROM_COURSE = "moodle_user_get_users_by_courseid";// core_enrol_get_enrolled_users()
 
 }
