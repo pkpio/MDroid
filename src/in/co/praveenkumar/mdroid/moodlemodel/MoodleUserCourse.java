8eda068691db85437a8edf47615f5457f6eaba5d
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-11-27 22:43:54 +0100

    Added new model for MoodleUserCourse

---
+++ src/in/co/praveenkumar/mdroid/moodlemodel/MoodleUserCourse.java
@@ -0,0 +1,90 @@
+package in.co.praveenkumar.mdroid.moodlemodel;
+
+import com.google.gson.annotations.SerializedName;
+import com.orm.SugarRecord;
+
+/**
+ * Represents course of any moodle user. Used in User profiles listing. A better
+ * way could be retreiving from logged in user account using courseid but the
+ * course may or may not be in his profile.
+ * 
+ * @author Praveen Kumar Pendyala<praveen@praveenkumar.co.in>
+ * 
+ */
+public class MoodleUserCourse extends SugarRecord<MoodleUserCourse> {
+	@SerializedName("id")
+	int courseid;
+
+	@SerializedName("fullname")
+	String fullname;
+
+	@SerializedName("shortname")
+	String shortname;
+
+	// Relational fields - for less logout complexity
+	long siteid;
+	long userid;
+
+	/**
+	 * Get id of the course
+	 * 
+	 * @return
+	 */
+	public int getCourseid() {
+		return courseid;
+	}
+
+	/**
+	 * Get course fullname
+	 * 
+	 * @return
+	 */
+	public String getFullname() {
+		return fullname;
+	}
+
+	/**
+	 * Get course shortname
+	 * 
+	 * @return
+	 */
+	public String getShortname() {
+		return shortname;
+	}
+
+	/**
+	 * Get siteid of this record
+	 * 
+	 * @return
+	 */
+	public long getSiteid() {
+		return siteid;
+	}
+
+	/**
+	 * Userid of user to whom this course belongs to
+	 * 
+	 * @return
+	 */
+	public long getUserid() {
+		return userid;
+	}
+
+	/**
+	 * Set siteid of this record
+	 * 
+	 * @return
+	 */
+	public void setSiteid(long siteid) {
+		this.siteid = siteid;
+	}
+
+	/**
+	 * Userid of user to whom this course belongs to
+	 * 
+	 * @return
+	 */
+	public void setUserid(long userid) {
+		this.userid = userid;
+	}
+}
