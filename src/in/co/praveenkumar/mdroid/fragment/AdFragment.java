c97f8449d11c0e8886784dfd27c23d058f1c14e8
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-12-09 20:04:47 +0100

    Ad states across interaction
    Former-commit-id: 9c2b6983449b400824b0ab5792752c959779b8b1

--- src/in/co/praveenkumar/mdroid/fragment/AdFragment.java
+++ src/in/co/praveenkumar/mdroid/fragment/AdFragment.java
@@ -11,6 +11,7 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 public class AdFragment extends Fragment {
+	AdView mAdView;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,8 +22,35 @@ public class AdFragment extends Fragment {
 	@Override
 	public void onActivityCreated(Bundle bundle) {
 		super.onActivityCreated(bundle);
-		AdView mAdView = (AdView) getView().findViewById(R.id.adView);
+		mAdView = (AdView) getView().findViewById(R.id.adView);
 		AdRequest adRequest = new AdRequest.Builder().build();
 		mAdView.loadAd(adRequest);
+	}
+
+	/** Called when leaving the activity */
+	@Override
+	public void onPause() {
+		if (mAdView != null) {
+			mAdView.pause();
+		}
+		super.onPause();
+	}
+
+	/** Called when returning to the activity */
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (mAdView != null) {
+			mAdView.resume();
+		}
+	}
+
+	/** Called before the activity is destroyed */
+	@Override
+	public void onDestroy() {
+		if (mAdView != null) {
+			mAdView.destroy();
+		}
+		super.onDestroy();
 	}
 }
