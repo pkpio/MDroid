6f0cf22e7715ec4c5b67dd710ceba1f28476d28e
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-12-12 16:49:14 +0100

    Adfragment updated with hiding ads for pro users
    Former-commit-id: 6f82ed095548b39ae17d700012b9620416ff5c42

--- src/in/co/praveenkumar/mdroid/fragment/AdFragment.java
+++ src/in/co/praveenkumar/mdroid/fragment/AdFragment.java
@@ -1,8 +1,11 @@
 package in.co.praveenkumar.mdroid.fragment;
 
 import in.co.praveenkumar.R;
+import in.co.praveenkumar.mdroid.helper.AppInterface.DonationInterface;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +14,9 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 public class AdFragment extends Fragment {
+	final String DEBUG_TAG = "AdFragment";
 	AdView mAdView;
+	DonationInterface donation;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,9 +25,28 @@ public class AdFragment extends Fragment {
 	}
 
 	@Override
+	public void onAttach(Activity a) {
+		super.onAttach(a);
+		try {
+			this.donation = (DonationInterface) a;
+		} catch (ClassCastException e) {
+			e.printStackTrace();
+			Log.d(DEBUG_TAG, a.toString()
+					+ " did not implement ForumIdInterface.");
+		}
+	}
+
+	@Override
 	public void onActivityCreated(Bundle bundle) {
 		super.onActivityCreated(bundle);
 		mAdView = (AdView) getView().findViewById(R.id.adView);
+
+		// Hide ads if he is a pro user
+		if (donation != null && donation.isProUser()) {
+			mAdView.setVisibility(AdView.GONE);
+			return;
+		}
+
 		AdRequest adRequest = new AdRequest.Builder().addTestDevice(
 				"B3EEABB8EE11C2BE770B684D95219ECB").build();
 		mAdView.loadAd(adRequest);
