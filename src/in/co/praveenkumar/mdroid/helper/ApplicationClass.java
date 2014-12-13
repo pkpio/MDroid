32105cfd5326e8e8e4884fa497af9ff9dc01ddea
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-12-13 23:04:03 +0100

    Analytics partially integrated Not reporting yet. It's all clumsy. Need to clean up and fix it.

--- src/in/co/praveenkumar/mdroid/helper/ApplicationClass.java
+++ src/in/co/praveenkumar/mdroid/helper/ApplicationClass.java
@@ -1,7 +1,44 @@
 package in.co.praveenkumar.mdroid.helper;
 
+import in.co.praveenkumar.R;
+
+import java.util.HashMap;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
 import com.orm.SugarApp;
 
 public class ApplicationClass extends SugarApp {
 
+	public ApplicationClass() {
+		super();
+	}
+
+	/**
+	 * Enum used to identify the tracker that needs to be used for tracking.
+	 * 
+	 * A single tracker is usually enough for most purposes. In case you do need
+	 * multiple trackers, storing them all in Application object helps ensure
+	 * that they are created only once per application instance.
+	 */
+	public enum TrackerName {
+		APP_TRACKER, // Tracker used only in this app.
+		GLOBAL_TRACKER, // Tracker used by all the apps from a company. eg:
+						// roll-up tracking.
+		ECOMMERCE_TRACKER, // Tracker used by all ecommerce transactions from a
+							// company.
+	}
+
+	HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
+
+	synchronized public Tracker getTracker(TrackerName trackerId) {
+		if (!mTrackers.containsKey(trackerId)) {
+
+			GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+			Tracker t = analytics.newTracker(R.xml.app_tracker);
+			mTrackers.put(trackerId, t);
+
+		}
+		return mTrackers.get(trackerId);
+	}
 }
