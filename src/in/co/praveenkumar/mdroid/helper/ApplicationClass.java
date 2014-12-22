bd5ccbaf2c4f4c335b5d5d032993a116ab8316f0
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-12-22 22:32:28 +0100

    SendScreen method added to ApplicationClass
    Former-commit-id: 3eb58e8e0d13fd7ddcc1ac4a92369ab373b68681

--- src/in/co/praveenkumar/mdroid/helper/ApplicationClass.java
+++ src/in/co/praveenkumar/mdroid/helper/ApplicationClass.java
@@ -3,6 +3,7 @@ package in.co.praveenkumar.mdroid.helper;
 import in.co.praveenkumar.R;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 import com.orm.SugarApp;
 
@@ -18,5 +19,25 @@ public class ApplicationClass extends SugarApp {
 		GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
 		mTracker = analytics.newTracker(R.xml.app_tracker);
 		return mTracker;
+	}
+
+	/**
+	 * Send a Google Analytics hit
+	 * 
+	 * @param ScreenName
+	 *            Name as it appears on the Analytics page
+	 */
+	public void sendScreen(String ScreenName) {
+		if (mTracker == null)
+			getTracker();
+
+		// Setup a tracker
+		Tracker t = mTracker;
+
+		// Set screen name.
+		t.setScreenName(ScreenName);
+
+		// Send a screen view.
+		t.send(new HitBuilders.AppViewBuilder().build());
 	}
 }
