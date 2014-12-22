f541d69972b7bc59681a8c42289e3fa150d33145
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-12-22 22:49:51 +0100

    Screen names updated in Params

--- src/in/co/praveenkumar/mdroid/helper/Param.java
+++ src/in/co/praveenkumar/mdroid/helper/Param.java
@@ -21,4 +21,25 @@ public class Param {
 	public static final String BILLING_FEATURE_UPLOADS_PID = "in.co.praveenkumar.mdroid.donate.features.uploading_files";
 	public static final String BILLING_LICENSE_KEY = "02425045829981832447";
 
+	/**************************************************
+	 * Google analytics related
+	 **************************************************/
+	// Screens
+	public static final String GA_SCREEN_TUTORIAL = "Tutorial";
+	public static final String GA_SCREEN_LOGIN = "Login";
+	public static final String GA_SCREEN_WEBSERVICESS_OFF = "WebServicesOff";
+	public static final String GA_SCREEN_COURSE = "Course";
+	public static final String GA_SCREEN_CONTENT = "Content";
+	public static final String GA_SCREEN_FORUM = "Forum";
+	public static final String GA_SCREEN_DISCUSSION = "Discussion";
+	public static final String GA_SCREEN_POST = "Posts";
+	public static final String GA_SCREEN_BROWSER = "Browser";
+	public static final String GA_SCREEN_CALENDAR = "Calendar";
+	public static final String GA_SCREEN_CONTACT = "Contact";
+	public static final String GA_SCREEN_MESSAGE_LISTING = "MessageListing";
+	public static final String GA_SCREEN_MESSAGING = "Messaging";
+	public static final String GA_SCREEN_DONATION = "Donation";
+	public static final String GA_SCREEN_WORKINPROGRESS = "WorkInProgress";
+	public static final String GA_SCREEN_SETTING = "Setting";
+
 }
