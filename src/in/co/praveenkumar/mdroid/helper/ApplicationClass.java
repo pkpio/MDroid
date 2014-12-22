ce2f26521abf2bf5fee362bc8f9ac253ab0f9a34
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-12-22 22:02:00 +0100

    Using single tracker
    Former-commit-id: d38460c65dc3c474d99424c62586f09bbd3f23a9

--- src/in/co/praveenkumar/mdroid/helper/ApplicationClass.java
+++ src/in/co/praveenkumar/mdroid/helper/ApplicationClass.java
@@ -2,8 +2,6 @@ package in.co.praveenkumar.mdroid.helper;
 
 import in.co.praveenkumar.R;
 
-import java.util.HashMap;
-
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
 import com.orm.SugarApp;
@@ -14,31 +12,11 @@ public class ApplicationClass extends SugarApp {
 		super();
 	}
 
-	/**
-	 * Enum used to identify the tracker that needs to be used for tracking.
-	 * 
-	 * A single tracker is usually enough for most purposes. In case you do need
-	 * multiple trackers, storing them all in Application object helps ensure
-	 * that they are created only once per application instance.
-	 */
-	public enum TrackerName {
-		APP_TRACKER, // Tracker used only in this app.
-		GLOBAL_TRACKER, // Tracker used by all the apps from a company. eg:
-						// roll-up tracking.
-		ECOMMERCE_TRACKER, // Tracker used by all ecommerce transactions from a
-							// company.
-	}
+	Tracker mTracker;
 
-	HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
-
-	synchronized public Tracker getTracker(TrackerName trackerId) {
-		if (!mTrackers.containsKey(trackerId)) {
-
-			GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-			Tracker t = analytics.newTracker(R.xml.app_tracker);
-			mTrackers.put(trackerId, t);
-
-		}
-		return mTrackers.get(trackerId);
+	synchronized public Tracker getTracker() {
+		GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+		mTracker = analytics.newTracker(R.xml.app_tracker);
+		return mTracker;
 	}
 }
