1bef83de830f339fb9900bcb77af0225e082072c
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-01-25 17:02:52 +0100

    New model MoodleException added

---
+++ src/in/co/praveenkumar/mdroid/moodlemodel/MoodleException.java
@@ -0,0 +1,32 @@
+package in.co.praveenkumar.mdroid.moodlemodel;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MoodleException {
+	@SerializedName("exception")
+	String exception;
+
+	@SerializedName("errorcode")
+	String errorcode;
+
+	@SerializedName("message")
+	String message;
+
+	@Override
+	public String toString() {
+		return "exception: " + exception + "\n\n errorcode: " + errorcode
+				+ "\n\n message: " + message;
+	}
+
+	public String getException() {
+		return exception;
+	}
+
+	public String getErrorcode() {
+		return errorcode;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
