705aca0bbd9e13ecec034fa6c98c15bcf9224f1c
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-01-31 05:37:59 +0100

    ScheduleReceiver added This sets up an Android alarm schedule for periodic triggering of our service using the StartServiceReceiver

---
+++ src/in/co/praveenkumar/mdroid/service/ScheduleReceiver.java
@@ -0,0 +1,45 @@
+package in.co.praveenkumar.mdroid.service;
+
+import java.util.Calendar;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ScheduleReceiver extends BroadcastReceiver {
+	private final String DEBUG_TAG = "MDroid Service";
+
+	// restart service every x hours
+	private long REPEAT_TIME = 1000 * 60 * 60;
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.d(DEBUG_TAG, "Service request received !");
+
+		// Get repeating frequency from database
+		REPEAT_TIME = REPEAT_TIME * 1;
+
+		AlarmManager service = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+
+		Intent i = new Intent(context, StartServiceReceiver.class);
+		PendingIntent pending = PendingIntent.getBroadcast(context, 0, i,
+				PendingIntent.FLAG_CANCEL_CURRENT);
+
+		Calendar cal = Calendar.getInstance();
+		// start 30 seconds after boot completed
+		cal.add(Calendar.SECOND, 30);
+
+		// Set first firing time to current time + frequency time
+		long startTime = cal.getTimeInMillis() + REPEAT_TIME;
+
+		// fetch every user setting time. Power optimized call.
+		service.setInexactRepeating(AlarmManager.RTC_WAKEUP, startTime,
+				REPEAT_TIME, pending);
+
+	}
+
+}
