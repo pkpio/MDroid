63c15fa258369e8a6aa941a49ad73ab4e9a73de9
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-02-02 19:15:39 +0100

    NotificationActivity handles siteid extras and switches account as needed
    Former-commit-id: 25f38401476adc314add240a1c783e7750bffe2d

--- src/in/co/praveenkumar/mdroid/activity/NotificationActivity.java
+++ src/in/co/praveenkumar/mdroid/activity/NotificationActivity.java
@@ -3,6 +3,7 @@ package in.co.praveenkumar.mdroid.activity;
 import in.co.praveenkumar.R;
 import in.co.praveenkumar.mdroid.helper.ApplicationClass;
 import in.co.praveenkumar.mdroid.helper.Param;
+import in.co.praveenkumar.mdroid.helper.SessionSetting;
 import android.os.Bundle;
 
 public class NotificationActivity extends BaseNavigationActivity {
@@ -10,6 +11,12 @@ public class NotificationActivity extends BaseNavigationActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		// Update current siteid based on notification clicked
+		Bundle extras = getIntent().getExtras();
+		if (extras != null)
+			new SessionSetting(this, extras.getLong("siteid", 1));
+
 		setContentView(R.layout.activity_notification);
 		setUpDrawer();
 
@@ -20,5 +27,4 @@ public class NotificationActivity extends BaseNavigationActivity {
 		getSupportActionBar().setTitle("Notifications");
 		getSupportActionBar().setIcon(R.drawable.icon_notifications_white);
 	}
-
 }
