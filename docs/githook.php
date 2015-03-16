eed2eb01cf09feb45aab298ed1e370e54178cf32
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-03-16 23:44:27 +0100

    Githook work complete

--- docs/githook.php
+++ docs/githook.php
@@ -1,14 +1,45 @@
 <?php
+  error_reporting(E_ALL);
+  $base_path = 'https://raw.githubusercontent.com/praveendath92/MDroid/master/';
+  $interest_path = 'docs/';
+
   $data = file_get_contents('php://input');
   $json = json_decode($data);
-  $head = $json->head_commit;
-  
-  $added = $head->added
-  $removed = $head->removed
-  $modified = $head->modified
+  $head = $json->{'head_commit'};
+
+  $added = $head->added;
+  $removed = $head->removed;
+  $modified = $head->modified;
 
   // Print added list
   foreach($added as $entry){
-    echo $entry;
+    if(startsWith($entry, $interest_path)){
+      $file = fopen(substr($entry, 5), w);
+      fwrite($file, file_get_contents($base_path . $entry));
+      fclose($file);
+    }
+  }
+
+  // Print removed list
+  foreach($removed as $entry){
+     if(startsWith($entry, $interest_path)){
+        unlink(substr($entry, 5));
+     }
+  }
+
+  // Print modified list
+  foreach($modified as $entry){
+    if(startsWith($entry, $interest_path)){
+      $file = fopen(substr($entry, 5), w);
+      fwrite($file, file_get_contents($base_path . $entry));
+      fclose($file);
+    }
+  }
+
+  /**
+   * Credit : SO
+   */
+  function startsWith($haystack, $needle) {
+    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
   }
 ?>
