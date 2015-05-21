9123c92967c16044061b5efecbb2f0dacc0587ec
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-05-21 18:24:54 +0200

    MessageListing issue partial fix

--- app/src/main/java/in/co/praveenkumar/mdroid/activity/MessagingActivity.java
+++ app/src/main/java/in/co/praveenkumar/mdroid/activity/MessagingActivity.java
@@ -1,37 +1,47 @@
 package in.co.praveenkumar.mdroid.activity;
 
 import in.co.praveenkumar.R;
+import in.co.praveenkumar.mdroid.fragment.MessageListingFragment;
 import in.co.praveenkumar.mdroid.helper.AppInterface.UserIdInterface;
 import in.co.praveenkumar.mdroid.helper.ApplicationClass;
 import in.co.praveenkumar.mdroid.helper.Param;
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
 public class MessagingActivity extends BaseNavigationActivity implements
-		UserIdInterface {
-	int userid;
+        UserIdInterface {
+    int userid;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_messaging);
-		setUpDrawer();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+        setUpDrawer();
 
-		// Send a tracker
-		((ApplicationClass) getApplication())
-				.sendScreen(Param.GA_SCREEN_MESSAGE_LISTING);
+        // Send a tracker
+        ((ApplicationClass) getApplication())
+                .sendScreen(Param.GA_SCREEN_MESSAGE_LISTING);
 
-		getSupportActionBar().setTitle("Messaging");
-		getSupportActionBar().setIcon(R.drawable.icon_message);
-	}
+        // Set fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager
+                .beginTransaction()
+                .replace(R.id.messaging_layout, new MessageListingFragment())
+                .commit();
 
-	@Override
-	public int getUserId() {
-		return this.userid;
-	}
+        getSupportActionBar().setTitle("Messaging");
+        getSupportActionBar().setIcon(R.drawable.icon_message);
+    }
 
-	@Override
-	public void setUserId(int userid) {
-		this.userid = userid;
-	}
+    @Override
+    public int getUserId() {
+        return this.userid;
+    }
+
+    @Override
+    public void setUserId(int userid) {
+        this.userid = userid;
+    }
 
 }
--- app/src/main/java/in/co/praveenkumar/mdroid/fragment/MessageListingFragment.java
+++ app/src/main/java/in/co/praveenkumar/mdroid/fragment/MessageListingFragment.java
@@ -1,6 +1,7 @@
 package in.co.praveenkumar.mdroid.fragment;
 
 import in.co.praveenkumar.R;
+import in.co.praveenkumar.mdroid.activity.MessagingActivity;
 import in.co.praveenkumar.mdroid.helper.AppInterface.UserIdInterface;
 import in.co.praveenkumar.mdroid.helper.LetterColor;
 import in.co.praveenkumar.mdroid.helper.SessionSetting;
@@ -18,6 +19,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.text.Html;
@@ -33,257 +35,260 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class MessageListingFragment extends Fragment implements
-		OnRefreshListener {
-	final String DEBUG_TAG = "MessageListingFragment";
-	List<ListMessage> messages = new ArrayList<MessageListingFragment.ListMessage>();
-	MessageListAdapter adapter;
-	SessionSetting session;
-	LinearLayout messagesEmptyLayout;
-	UserIdInterface useridInterface;
-	SwipeRefreshLayout swipeLayout;
+        OnRefreshListener {
+    final String DEBUG_TAG = "MessageListingFragment";
+    List<ListMessage> messages = new ArrayList<MessageListingFragment.ListMessage>();
+    FragmentManager mFragmentManager;
+    MessageListAdapter adapter;
+    SessionSetting session;
+    LinearLayout messagesEmptyLayout;
+    UserIdInterface useridInterface;
+    SwipeRefreshLayout swipeLayout;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-		View rootView = inflater.inflate(R.layout.frag_message_listing,
-				container, false);
-		messagesEmptyLayout = (LinearLayout) rootView
-				.findViewById(R.id.messages_empty_layout);
-		ListView messageList = (ListView) rootView
-				.findViewById(R.id.content_message_listing);
+        View rootView = inflater.inflate(R.layout.frag_message_listing,
+                container, false);
+        messagesEmptyLayout = (LinearLayout) rootView
+                .findViewById(R.id.messages_empty_layout);
+        ListView messageList = (ListView) rootView
+                .findViewById(R.id.content_message_listing);
 
-		// Setup siteinfo and messages
-		session = new SessionSetting(getActivity());
-		setupMessages();
+        // Setup siteinfo and messages
+        session = new SessionSetting(getActivity());
+        setupMessages();
 
-		// Setup list adapter
-		adapter = new MessageListAdapter(getActivity());
-		messageList.setAdapter(adapter);
+        // Setup list adapter
+        adapter = new MessageListAdapter(getActivity());
+        messageList.setAdapter(adapter);
 
-		// itemclick listener
-		messageList.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				useridInterface.setUserId(messages.get(position).userid);
-				getFragmentManager()
-						.beginTransaction()
-						.addToBackStack(null)
-						.replace(R.id.messaging_layout, new MessagingFragment())
-						.commit();
-			}
-		});
+        // itemclick listener
+        messageList.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                useridInterface.setUserId(messages.get(position).userid);
+                mFragmentManager
+                        .beginTransaction()
+                        .addToBackStack(null)
+                        .replace(R.id.messaging_layout, new MessagingFragment())
+                        .commit();
+                mFragmentManager.executePendingTransactions();
+            }
+        });
 
-		swipeLayout = (SwipeRefreshLayout) rootView
-				.findViewById(R.id.swipe_refresh);
-		Workaround.linkSwipeRefreshAndListView(swipeLayout, messageList);
-		swipeLayout.setOnRefreshListener(this);
+        swipeLayout = (SwipeRefreshLayout) rootView
+                .findViewById(R.id.swipe_refresh);
+        Workaround.linkSwipeRefreshAndListView(swipeLayout, messageList);
+        swipeLayout.setOnRefreshListener(this);
 
-		new MessageSyncerBg().execute("");
+        new MessageSyncerBg().execute("");
 
-		return rootView;
-	}
+        return rootView;
+    }
 
-	@Override
-	public void onAttach(Activity a) {
-		super.onAttach(a);
-		try {
-			useridInterface = (UserIdInterface) a;
-		} catch (ClassCastException e) {
-			e.printStackTrace();
-			Log.d(DEBUG_TAG, a.toString()
-					+ " did not implement UserIdInterface.");
-		}
-	}
+    @Override
+    public void onAttach(Activity a) {
+        super.onAttach(a);
+        try {
+            useridInterface = (UserIdInterface) a;
+            mFragmentManager = ((MessagingActivity) a).getSupportFragmentManager();
+        } catch (ClassCastException e) {
+            e.printStackTrace();
+            Log.d(DEBUG_TAG, a.toString()
+                    + " did not implement UserIdInterface.");
+        }
+    }
 
-	private class MessageSyncerBg extends AsyncTask<String, Integer, Boolean> {
+    private class MessageSyncerBg extends AsyncTask<String, Integer, Boolean> {
 
-		@Override
-		protected void onPreExecute() {
-			swipeLayout.setRefreshing(true);
-		}
+        @Override
+        protected void onPreExecute() {
+            swipeLayout.setRefreshing(true);
+        }
 
-		@Override
-		protected Boolean doInBackground(String... params) {
-			// Sync from server and update
-			MessageSyncTask mst = new MessageSyncTask(session.getmUrl(),
-					session.getToken(), session.getCurrentSiteId());
-			if (mst.syncMessages(session.getSiteInfo().getUserid()))
-				return true;
-			else
-				return false;
-		}
+        @Override
+        protected Boolean doInBackground(String... params) {
+            // Sync from server and update
+            MessageSyncTask mst = new MessageSyncTask(session.getmUrl(),
+                    session.getToken(), session.getCurrentSiteId());
+            if (mst.syncMessages(session.getSiteInfo().getUserid()))
+                return true;
+            else
+                return false;
+        }
 
-		@Override
-		protected void onPostExecute(Boolean result) {
-			setupMessages();
-			adapter.notifyDataSetChanged();
-			swipeLayout.setRefreshing(false);
-		}
+        @Override
+        protected void onPostExecute(Boolean result) {
+            setupMessages();
+            adapter.notifyDataSetChanged();
+            swipeLayout.setRefreshing(false);
+        }
 
-	}
+    }
 
-	public class MessageListAdapter extends BaseAdapter {
+    public class MessageListAdapter extends BaseAdapter {
 
-		private final Context context;
+        private final Context context;
 
-		public MessageListAdapter(Context context) {
-			this.context = context;
-			if (messages == null || messages.size() == 0)
-				messagesEmptyLayout.setVisibility(LinearLayout.VISIBLE);
-			else
-				messagesEmptyLayout.setVisibility(LinearLayout.GONE);
-		}
+        public MessageListAdapter(Context context) {
+            this.context = context;
+            if (messages == null || messages.size() == 0)
+                messagesEmptyLayout.setVisibility(LinearLayout.VISIBLE);
+            else
+                messagesEmptyLayout.setVisibility(LinearLayout.GONE);
+        }
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			ViewHolder viewHolder;
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
 
-			if (convertView == null) {
-				viewHolder = new ViewHolder();
-				LayoutInflater inflater = (LayoutInflater) context
-						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null) {
+                viewHolder = new ViewHolder();
+                LayoutInflater inflater = (LayoutInflater) context
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-				convertView = inflater.inflate(R.layout.list_item_message_list,
-						parent, false);
+                convertView = inflater.inflate(R.layout.list_item_message_list,
+                        parent, false);
 
-				viewHolder.userimage = (TextView) convertView
-						.findViewById(R.id.list_message_contact_image);
-				viewHolder.userfullname = (TextView) convertView
-						.findViewById(R.id.list_message_contact_name);
-				viewHolder.lastmessage = (TextView) convertView
-						.findViewById(R.id.list_message_last_message);
+                viewHolder.userimage = (TextView) convertView
+                        .findViewById(R.id.list_message_contact_image);
+                viewHolder.userfullname = (TextView) convertView
+                        .findViewById(R.id.list_message_contact_name);
+                viewHolder.lastmessage = (TextView) convertView
+                        .findViewById(R.id.list_message_last_message);
 
-				convertView.setTag(viewHolder);
-			} else {
-				viewHolder = (ViewHolder) convertView.getTag();
-			}
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
 
-			// Contact image color and value
-			String name = messages.get(position).userfullname;
-			char firstChar = 0;
-			if (name.length() != 0)
-				firstChar = name.charAt(0);
-			viewHolder.userimage.setText(firstChar + "");
-			viewHolder.userimage.setBackgroundColor(LetterColor.of(firstChar));
+            // Contact image color and value
+            String name = messages.get(position).userfullname;
+            char firstChar = 0;
+            if (name.length() != 0)
+                firstChar = name.charAt(0);
+            viewHolder.userimage.setText(firstChar + "");
+            viewHolder.userimage.setBackgroundColor(LetterColor.of(firstChar));
 
-			// Name
-			viewHolder.userfullname
-					.setText(messages.get(position).userfullname);
+            // Name
+            viewHolder.userfullname
+                    .setText(messages.get(position).userfullname);
 
-			// Last message after trimming html special chars
-			String msg = messages.get(position).message.getText();
-			msg = (msg == null) ? "" : Html.fromHtml(msg).toString().trim();
-			viewHolder.lastmessage.setText(msg);
+            // Last message after trimming html special chars
+            String msg = messages.get(position).message.getText();
+            msg = (msg == null) ? "" : Html.fromHtml(msg).toString().trim();
+            viewHolder.lastmessage.setText(msg);
 
-			return convertView;
-		}
+            return convertView;
+        }
 
-		@Override
-		public int getCount() {
-			if (messages == null)
-				return 0;
-			return messages.size();
-		}
+        @Override
+        public int getCount() {
+            if (messages == null)
+                return 0;
+            return messages.size();
+        }
 
-		@Override
-		public Object getItem(int position) {
-			return messages.get(position);
-		}
+        @Override
+        public Object getItem(int position) {
+            return messages.get(position);
+        }
 
-		@Override
-		public long getItemId(int position) {
-			return position;
-		}
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
 
-		@Override
-		public void notifyDataSetChanged() {
-			if (messages.size() != 0)
-				messagesEmptyLayout.setVisibility(LinearLayout.GONE);
-			super.notifyDataSetChanged();
-		}
-	}
+        @Override
+        public void notifyDataSetChanged() {
+            if (messages.size() != 0)
+                messagesEmptyLayout.setVisibility(LinearLayout.GONE);
+            super.notifyDataSetChanged();
+        }
+    }
 
-	static class ViewHolder {
-		TextView userimage;
-		TextView userfullname;
-		TextView lastmessage;
-	}
+    static class ViewHolder {
+        TextView userimage;
+        TextView userfullname;
+        TextView lastmessage;
+    }
 
-	void setupMessages() {
-		List<MoodleMessage> mMessages = MoodleMessage.find(MoodleMessage.class,
-				"siteid = ?", session.getCurrentSiteId() + "");
-		List<ListMessage> lMessages = new ArrayList<MessageListingFragment.ListMessage>();
+    void setupMessages() {
+        List<MoodleMessage> mMessages = MoodleMessage.find(MoodleMessage.class,
+                "siteid = ?", session.getCurrentSiteId() + "");
+        List<ListMessage> lMessages = new ArrayList<MessageListingFragment.ListMessage>();
 
-		// Sort messages with newest first in list
-		Collections.sort(mMessages, new Comparator<MoodleMessage>() {
-			public int compare(MoodleMessage m1, MoodleMessage m2) {
-				if (m1.getTimecreated() == m2.getTimecreated())
-					return 0;
-				return m1.getTimecreated() < m2.getTimecreated() ? 1 : -1;
-			}
-		});
+        // Sort messages with newest first in list
+        Collections.sort(mMessages, new Comparator<MoodleMessage>() {
+            public int compare(MoodleMessage m1, MoodleMessage m2) {
+                if (m1.getTimecreated() == m2.getTimecreated())
+                    return 0;
+                return m1.getTimecreated() < m2.getTimecreated() ? 1 : -1;
+            }
+        });
 
-		List<Integer> userids = new ArrayList<Integer>();
-		int currentuserid = session.getSiteInfo().getUserid();
+        List<Integer> userids = new ArrayList<Integer>();
+        int currentuserid = session.getSiteInfo().getUserid();
 
-		for (int i = 0; i < mMessages.size(); i++) {
+        for (int i = 0; i < mMessages.size(); i++) {
 
-			// Message sent by current user
-			if (currentuserid != mMessages.get(i).getUseridto()
-					&& !isInList(userids, mMessages.get(i).getUseridto())) {
-				ListMessage mes = new ListMessage();
-				mes.message = mMessages.get(i);
-				mes.userid = mMessages.get(i).getUseridto();
-				mes.userfullname = mMessages.get(i).getUsertofullname();
-				lMessages.add(mes);
-				userids.add(mMessages.get(i).getUseridto());
-			}
+            // Message sent by current user
+            if (currentuserid != mMessages.get(i).getUseridto()
+                    && !isInList(userids, mMessages.get(i).getUseridto())) {
+                ListMessage mes = new ListMessage();
+                mes.message = mMessages.get(i);
+                mes.userid = mMessages.get(i).getUseridto();
+                mes.userfullname = mMessages.get(i).getUsertofullname();
+                lMessages.add(mes);
+                userids.add(mMessages.get(i).getUseridto());
+            }
 
-			// Message received by current user
-			else if (currentuserid != mMessages.get(i).getUseridfrom()
-					&& !isInList(userids, mMessages.get(i).getUseridfrom())) {
-				ListMessage mes = new ListMessage();
-				mes.message = mMessages.get(i);
-				mes.userid = mMessages.get(i).getUseridfrom();
-				mes.userfullname = mMessages.get(i).getUserfromfullname();
-				lMessages.add(mes);
-				userids.add(mMessages.get(i).getUseridfrom());
-			}
-		}
-		messages = (lMessages != null) ? lMessages
-				: new ArrayList<MessageListingFragment.ListMessage>();
-	}
+            // Message received by current user
+            else if (currentuserid != mMessages.get(i).getUseridfrom()
+                    && !isInList(userids, mMessages.get(i).getUseridfrom())) {
+                ListMessage mes = new ListMessage();
+                mes.message = mMessages.get(i);
+                mes.userid = mMessages.get(i).getUseridfrom();
+                mes.userfullname = mMessages.get(i).getUserfromfullname();
+                lMessages.add(mes);
+                userids.add(mMessages.get(i).getUseridfrom());
+            }
+        }
+        messages = (lMessages != null) ? lMessages
+                : new ArrayList<MessageListingFragment.ListMessage>();
+    }
 
-	Boolean isInList(List<Integer> ids, int id) {
-		for (int i = 0; i < ids.size(); i++)
-			if (ids.get(i).intValue() == id)
-				return true;
-		return false;
-	}
+    Boolean isInList(List<Integer> ids, int id) {
+        for (int i = 0; i < ids.size(); i++)
+            if (ids.get(i).intValue() == id)
+                return true;
+        return false;
+    }
 
-	class ListMessage {
-		/**
-		 * Moodle message
-		 */
-		MoodleMessage message;
-		/**
-		 * userid of the user, other the current login user, who is
-		 * participating in this message
-		 */
-		int userid;
+    class ListMessage {
+        /**
+         * Moodle message
+         */
+        MoodleMessage message;
+        /**
+         * userid of the user, other the current login user, who is
+         * participating in this message
+         */
+        int userid;
 
-		/**
-		 * Fullname of the user, other the current login user, who is
-		 * participating in this message
-		 */
-		String userfullname;
-	}
+        /**
+         * Fullname of the user, other the current login user, who is
+         * participating in this message
+         */
+        String userfullname;
+    }
 
-	@Override
-	public void onRefresh() {
-		new MessageSyncerBg().execute("");
-	}
+    @Override
+    public void onRefresh() {
+        new MessageSyncerBg().execute("");
+    }
 
 }
