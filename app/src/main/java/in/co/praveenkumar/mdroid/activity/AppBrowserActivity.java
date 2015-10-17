031f42fca066a089c8029bf959f68259736857fe
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-10-17 03:09:35 +0200

    Auto login feature tested. Working!

--- app/src/main/java/in/co/praveenkumar/mdroid/activity/AppBrowserActivity.java
+++ app/src/main/java/in/co/praveenkumar/mdroid/activity/AppBrowserActivity.java
@@ -14,9 +14,12 @@ import android.webkit.WebViewClient;
 @SuppressWarnings("deprecation")
 @SuppressLint("SetJavaScriptEnabled")
 public class AppBrowserActivity extends BaseNavigationActivity {
+    final int MAX_LOGIN_ATTEMPTS = 2; // Since page load callback occurs during redirects too.
+
 	WebView mBrowser;
 	String DEFAULT_URL = "http://mdroid.praveenkumar.co.in";
 	String DEFAULT_TITLE = "MDroid browser";
+    int loginAttempts = 0;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -46,33 +49,38 @@ public class AppBrowserActivity extends BaseNavigationActivity {
 		mBrowser.getSettings().setJavaScriptEnabled(true);
 		mBrowser.getSettings().setDomStorageEnabled(true);
 		mBrowser.setWebChromeClient(new WebChromeClient() {
-			@Override
-			public boolean onJsAlert(WebView view, String url, String message,
-					JsResult result) {
-				return super.onJsAlert(view, url, message, result);
-			}
-		});
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message,
+                                     JsResult result) {
+                return super.onJsAlert(view, url, message, result);
+            }
+        });
 
 		mBrowser.setWebViewClient(new WebViewClient() {
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				view.loadUrl(url);
-				// Login user if not logged in
-				// if (view.getUrl() == "") {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
 
-				// }
-				return true;
-			}
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                System.out.println("Page loaded!");
+                if (loginAttempts < MAX_LOGIN_ATTEMPTS) {
+                    loginAttempts++;
+                    String uname = "demo";
+                    String password = "demo";
+                    view.loadUrl("javascript: {" +
+                            "document.getElementById('username').value = '" + uname + "';" +
+                            "document.getElementById('password').value = '" + password + "';" +
+                            "document.forms[0].submit(); };");
+                    System.out.println("Login tried");
+                }
+            }
+        });
 
-			@Override
-			public void onPageFinished(WebView view, String url) {
-				// view.loadUrl("javascript:"
-				// + "document.getElementById('password').value='lola';");
-				System.out.println("Page loaded!");
-			}
-		});
-
-		mBrowser.loadUrl(url);
+        loginAttempts = 0;
+        mBrowser.loadUrl(url);
 	}
 
 }
